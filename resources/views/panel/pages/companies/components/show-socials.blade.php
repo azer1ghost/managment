4383ace29656a94a0dea3e2210c74a1c3ce9a1b4
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-08-19 09:50:46 +0400

    Delete social from company

--- resources/views/panel/pages/companies/components/show-socials.blade.php
+++ resources/views/panel/pages/companies/components/show-socials.blade.php
@@ -13,13 +13,11 @@
                         <input type="hidden"  name="socials[{{$index}}][id]"    value="{{$social['id']}}">
                         <x-input::select      name="socials[{{$index}}][name]" :value="$social['name']"  label="Social name" width="5" :options="$socialNetworks"/>
                         <x-input::text        name="socials[{{$index}}][url]"  :value="$social['url']"   label="Social url"  width="6"  class="pr-3"/>
-{{--                        @if (!$social['id'])--}}
-                            <div class="form-group col-12 col-md-1 mb-3 mb-md-0 mt-0 mt-md-3 pl-3 pl-md-0">
-                                <button type="button" wire:click.prevent="removeSocial({{$index}})" class="btn btn-outline-danger">
-                                    <i class='fal fa-times'></i>
-                                </button>
-                            </div>
-{{--                        @endif--}}
+                        <div class="form-group col-12 col-md-1 mb-3 mb-md-0 mt-0 mt-md-3 pl-3 pl-md-0">
+                            <button type="button" wire:click.prevent="removeSocial({{$index}})" class="btn btn-outline-danger">
+                                <i class='fal fa-times'></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             @empty
