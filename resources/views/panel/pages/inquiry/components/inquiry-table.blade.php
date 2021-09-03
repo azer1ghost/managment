563e7b67246e2ce2b35b1ee0af5bcb04223bcf8e
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-09-03 09:28:53 +0400

    some translates and changing the inquiry status on the fly done v2

--- resources/views/panel/pages/inquiry/components/inquiry-table.blade.php
+++ resources/views/panel/pages/inquiry/components/inquiry-table.blade.php
@@ -150,7 +150,7 @@
                         style="position: absolute;
                                top: 0;left: 0;
                                width: 100%;height: 100%;
-                               background: rgba(0, 0, 0, 0.5);z-index: 999;"
+                               background: rgba(0, 0, 0, 0.3);z-index: 999;"
                 >
                     <h3 class="text-white">@lang('translates.loading')...</h3>
                 </div>
