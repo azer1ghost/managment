1aac9760a38af16ec965cd888f8d62d07f36dabb
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-09-06 12:14:22 +0400

    Permissions for user done v1

--- resources/views/panel/pages/users/edit.blade.php
+++ resources/views/panel/pages/users/edit.blade.php
@@ -71,45 +71,49 @@
                                     <x-input::text type="password" name="password_confirmation" width="6" class="pr-1" label="Password Confirmation" autocomplete="off"/>
                                 @endif
                                 <x-input::select  name="role_id"   :value="optional($data)->getRelationValue('role')->getAttribute('id')"  width="3" class="pr-1" :options="$roles" label="Role"/>
-                                <p class="text-muted mb-2">PERMISSIONS</p>
-                                <p class="text-muted my-2">All</p>
-                                <div class="form-check">
-                                    <input class="form-check-input" @if (Str::of(optional($data)->getAttribute('permissions'))->trim() == 'all')) checked @endif type="checkbox" name="all_perms" value="all" id="perm-0">
-                                    <label class="form-check-label" for="perm-0">
-                                        All
-                                    </label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="check-perms">
-                                    <label class="form-check-label" for="check-perms">
-                                        Choose All
-                                    </label>
-                                </div>
-                                @error("all_perms") <p class="text-danger">{{$message}}</p> @enderror
-                                <div class="row">
-                                    @php $perms = config('auth.permissions') @endphp
-                                    @foreach ($perms as $index => $perm)
-                                        @php
-                                            // next and previous permissions
-                                            $prevPerm = $perms[$index == 0 ?: $index - 1];
-                                            $nextPerm = $perms[$index == $loop->count - 1 ?: $index + 1];
-                                            // type of permission
-                                            $type  = strpos($perm, '-') ? substr($perm, strpos($perm, '-') + 1) : $perm;
-                                        @endphp
-                                        @if (!Str::contains($prevPerm, $type) || $loop->first)
-                                            <div class="col-12 col-md-4 my-2">
-                                                <p class="text-muted my-2">{{ucfirst($type)}}</p>
-                                                @endif
-                                                <div class="form-check">
-                                                    <input class="form-check-input" @if (Str::contains(optional($data)->getAttribute('permissions'),$perm)) checked @endif type="checkbox" name="perms[]" value="{{$perm}}" id="perm-{{$loop->iteration}}">
-                                                    <label class="form-check-label" for="perm-{{$loop->iteration}}">
-                                                        {{$perm}}
-                                                    </label>
-                                                </div>
-                                                @if (!Str::contains($nextPerm, $type) || $loop->first) </div> @endif
-                                    @endforeach
-                                </div>
-                                @error("perms") <p class="text-danger">{{$message}}</p> @enderror
+                                @if(auth()->user()->isDeveloper() || auth()->user()->isAdministrator())
+                                    <div class="col-md-12">
+                                        <p class="text-muted mb-2">PERMISSIONS</p>
+                                        <p class="text-muted my-2">All</p>
+                                        <div class="form-check">
+                                            <input class="form-check-input" @if (Str::of(optional($data)->getAttribute('permissions'))->trim() == 'all')) checked @endif type="checkbox" name="all_perms" value="all" id="perm-0">
+                                            <label class="form-check-label" for="perm-0">
+                                                All
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" id="check-perms">
+                                            <label class="form-check-label" for="check-perms">
+                                                Choose All
+                                            </label>
+                                        </div>
+                                        @error("all_perms") <p class="text-danger">{{$message}}</p> @enderror
+                                        <div class="row">
+                                            @php $perms = config('auth.permissions') @endphp
+                                            @foreach ($perms as $index => $perm)
+                                                @php
+                                                    // next and previous permissions
+                                                    $prevPerm = $perms[$index == 0 ?: $index - 1];
+                                                    $nextPerm = $perms[$index == $loop->count - 1 ?: $index + 1];
+                                                    // type of permission
+                                                    $type  = strpos($perm, '-') ? substr($perm, strpos($perm, '-') + 1) : $perm;
+                                                @endphp
+                                                @if (!Str::contains($prevPerm, $type) || $loop->first)
+                                                    <div class="col-12 col-md-4 my-2">
+                                                        <p class="text-muted my-2">{{ucfirst($type)}}</p>
+                                                        @endif
+                                                        <div class="form-check">
+                                                            <input class="form-check-input" @if (Str::contains(optional($data)->getAttribute('permissions'),$perm)) checked @endif type="checkbox" name="perms[]" value="{{$perm}}" id="perm-{{$loop->iteration}}">
+                                                            <label class="form-check-label" for="perm-{{$loop->iteration}}">
+                                                                {{$perm}}
+                                                            </label>
+                                                        </div>
+                                                        @if (!Str::contains($nextPerm, $type) || $loop->first) </div> @endif
+                                            @endforeach
+                                        </div>
+                                        @error("perms") <p class="text-danger">{{$message}}</p> @enderror
+                                    </div>
+                                @endif
                                 <div class="col-md-12">
                                     <br>
                                     <p class="text-muted mb-2">USER DEFAULTS</p>
