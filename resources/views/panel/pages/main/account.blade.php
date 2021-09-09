51b883922545c91d1ff47e3cf21a5b9fcb2f9f99
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-09-09 09:47:11 +0400

    account position fixed

--- resources/views/panel/pages/main/account.blade.php
+++ resources/views/panel/pages/main/account.blade.php
@@ -30,7 +30,7 @@
                                 <p class="text-muted mb-2">EMPLOYMENT</p>
                                 <hr class="my-2">
                                 <div class="row">
-                                    <x-input::select  name="position_id"   :value="auth()->user()->getRelationValue('position')->getAttribute('id')"   width="6"  class="pr-1" :options="$positions" label="Position" />
+                                    <x-input::select  name="position_id"   :value="optional(auth()->user()->getRelationValue('position'))->getAttribute('id')"   width="6"  class="pr-1" :options="$positions" label="Position" />
                                     <x-input::select  name="department_id" :value="auth()->user()->getRelationValue('department')->getAttribute('id')" width="6"  class="pr-1" :options="$departments" label="Department" />
                                     <x-input::select  name="company_id"    :value="auth()->user()->getRelationValue('company')->getAttribute('id')"  width="6"  class="pr-1"   :options="$companies" label="Company" />
                                     @if (auth()->user()->isDeveloper())
