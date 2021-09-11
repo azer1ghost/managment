494f9e05e208a79e4a28e8a6e072626e04e6fa34
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-09-11 14:56:06 +0400

    Task beta version v2

--- app/Models/User.php
+++ app/Models/User.php
@@ -89,7 +89,7 @@ class User extends Authenticatable implements MustVerifyPhone
 
     public function hasPermission($perm): bool
     {
-        if (app()->environment('111')){
+        if (app()->environment('local')){
             $permissions = config('auth.permissions');
         }else{
             $permissions = explode(',',
