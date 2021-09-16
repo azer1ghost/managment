d0d98e23b5b9ff995550f4e5e74cd1686f32d0fe
Azer <mamedovazer124@gmail.com>
2021-09-16 16:49:27 +0400

    Revert "task comments"
    This reverts commit f9e64a1fdff2d19d4377760353107f5afad7e29e.

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
