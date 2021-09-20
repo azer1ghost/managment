2f723aca9b7cea30eb37fd9fa5c3d7d41721f75c
Azer <mamedovazer124@gmail.com>
2021-09-20 10:31:24 +0400

    bugsnag integration

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -230,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Bugsnag' => Bugsnag\BugsnagLaravel\Facades\Bugsnag::class,
     ],
 
 ];
