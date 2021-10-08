2db635bc3512ed8d0624449401abb46419a055dd
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-10-08 12:25:51 +0400

    InquiryStatusWidget.php done v2

--- resources/views/components/widgets/inquiry-daily-widget.blade.php
+++ resources/views/components/widgets/inquiry-daily-widget.blade.php
@@ -1,3 +1,39 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
+<div class="{{$widget->class_attribute}}">
+    <div class="card">
+        <div class="py-2 px-1">
+            <div id="{{$widget->key}}" style="width: 100%; height: 300px;"></div>
+            <script>
+                const inquiryDailyChart = am4core.create("{{$widget->key}}", am4charts.XYChart);
+                const inquiryDailyData = @json($results);
+                const inquiryDailyKeys = @json($keys);
+                {{--const colors = @json($colors);--}}
+                const inquiryDailyOverall = [];
+                inquiryDailyData.forEach(function (value, idx){
+                    inquiryDailyOverall.push({'daily': inquiryDailyKeys[idx], 'value': value})
+                });
+                inquiryDailyChart.data = inquiryDailyOverall;
+                inquiryDailyChart.innerRadius = am4core.percent(40);
+                inquiryDailyChart.radius = am4core.percent(90);
+                inquiryDailyChart.legend = new am4charts.Legend();
+                inquiryDailyChart.legend.position = "right";
+                const inquiryDailyTitle = inquiryDailyChart.titles.create();
+                inquiryDailyTitle.text = '{{$widget->details}}';
+                inquiryDailyTitle.fontSize = 25;
+                inquiryDailyTitle.marginBottom = 0;
+                const inquiryDailyPieSeries = inquiryDailyChart.series.push(new am4charts.PieSeries());
+                inquiryDailyPieSeries.dataFields.value = "daily";
+                inquiryDailyPieSeries.dataFields.category = "status";
+                // pieSeries.slices.template.propertyFields.fill = "color";
+                // Disable ticks and labels
+                inquiryDailyPieSeries.labels.template.disabled = true;
+                inquiryDailyPieSeries.ticks.template.disabled = true;
+                const inquiryDailyRgm = new am4core.RadialGradientModifier();
+                inquiryDailyRgm.brightnesses.push(-0.8, -0.8, -0.5, 0, - 0.5);
+                inquiryDailyPieSeries.slices.template.fillModifier = inquiryDailyRgm;
+                inquiryDailyPieSeries.slices.template.strokeModifier = inquiryDailyRgm;
+                inquiryDailyPieSeries.slices.template.strokeOpacity = 0.4;
+                inquiryDailyPieSeries.slices.template.strokeWidth = 0;
+            </script>
+        </div>
+    </div>
 </div>
--- resources/views/components/widgets/inquiry-status-widget.blade.php
+++ resources/views/components/widgets/inquiry-status-widget.blade.php
@@ -3,36 +3,36 @@
         <div class="py-2 px-1">
             <div id="{{$widget->key}}" style="width: 100%; height: 300px;"></div>
             <script>
-                const chart = am4core.create("{{$widget->key}}", am4charts.PieChart);
-                const data = @json($results);
-                const keys = @json($keys);
+                const inquiryStatusChart = am4core.create("{{$widget->key}}", am4charts.PieChart);
+                const inquiryStatusData = @json($results);
+                const inquiryStatusKeys = @json($keys);
                 {{--const colors = @json($colors);--}}
-                const overall = [];
-                data.forEach(function (value, idx){
-                    overall.push({'status': keys[idx], 'value': value})
+                const inquiryStatusOverall = [];
+                inquiryStatusData.forEach(function (value, idx){
+                    inquiryStatusOverall.push({'status': inquiryStatusKeys[idx], 'value': value})
                 });
-                chart.data = overall;
-                chart.innerRadius = am4core.percent(40);
-                chart.radius = am4core.percent(90);
-                chart.legend = new am4charts.Legend();
-                chart.legend.position = "right";
-                const title = chart.titles.create();
-                title.text = '{{$widget->details}}';
-                title.fontSize = 25;
-                title.marginBottom = 0;
-                const pieSeries = chart.series.push(new am4charts.PieSeries());
-                pieSeries.dataFields.value = "value";
-                pieSeries.dataFields.category = "status";
-                pieSeries.slices.template.propertyFields.fill = "color";
+                inquiryStatusChart.data = inquiryStatusOverall;
+                inquiryStatusChart.innerRadius = am4core.percent(40);
+                inquiryStatusChart.radius = am4core.percent(90);
+                inquiryStatusChart.legend = new am4charts.Legend();
+                inquiryStatusChart.legend.position = "right";
+                const inquiryStatusTitle = inquiryStatusChart.titles.create();
+                inquiryStatusTitle.text = '{{$widget->details}}';
+                inquiryStatusTitle.fontSize = 25;
+                inquiryStatusTitle.marginBottom = 0;
+                const inquiryStatusPieSeries = inquiryStatusChart.series.push(new am4charts.PieSeries());
+                inquiryStatusPieSeries.dataFields.value = "value";
+                inquiryStatusPieSeries.dataFields.category = "status";
+                // pieSeries.slices.template.propertyFields.fill = "color";
                 // Disable ticks and labels
-                pieSeries.labels.template.disabled = true;
-                pieSeries.ticks.template.disabled = true;
-                const rgm = new am4core.RadialGradientModifier();
-                rgm.brightnesses.push(-0.8, -0.8, -0.5, 0, - 0.5);
-                pieSeries.slices.template.fillModifier = rgm;
-                pieSeries.slices.template.strokeModifier = rgm;
-                pieSeries.slices.template.strokeOpacity = 0.4;
-                pieSeries.slices.template.strokeWidth = 0;
+                inquiryStatusPieSeries.labels.template.disabled = true;
+                inquiryStatusPieSeries.ticks.template.disabled = true;
+                const inquiryStatusRgm = new am4core.RadialGradientModifier();
+                inquiryStatusRgm.brightnesses.push(-0.8, -0.8, -0.5, 0, - 0.5);
+                inquiryStatusPieSeries.slices.template.fillModifier = inquiryStatusRgm;
+                inquiryStatusPieSeries.slices.template.strokeModifier = inquiryStatusRgm;
+                inquiryStatusPieSeries.slices.template.strokeOpacity = 0.4;
+                inquiryStatusPieSeries.slices.template.strokeWidth = 0;
             </script>
         </div>
     </div>
