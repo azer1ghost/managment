6b127918f7a974d8710bb4951bb15916d3db4d58
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-10-18 23:05:23 +0400

    InquiryDailyWidget.php updated

--- app/View/Components/Widgets/InquiryDailyWidget.php
+++ app/View/Components/Widgets/InquiryDailyWidget.php
@@ -22,7 +22,7 @@ class InquiryDailyWidget extends Component
         $this->model = $this->getClassRealName();
 
         $this->results = ['data' => Inquiry::isReal()->select('id', 'datetime')
-            ->where('datetime', '>=', Carbon::now()->subDays(6))
+            ->where('datetime', '>=', Carbon::now()->subWeek())
             ->get()
             ->groupBy(function($date) {
                 return Carbon::parse($date->datetime)->format('m-d-Y');
