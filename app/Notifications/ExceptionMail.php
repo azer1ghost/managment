52112a977381b661f2608c8174088346fd930380
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-10-18 11:24:36 +0400

    user ip and device added to report error

--- app/Notifications/ExceptionMail.php
+++ app/Notifications/ExceptionMail.php
@@ -4,6 +4,7 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\Request;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\HtmlString;
@@ -12,12 +13,14 @@ class ExceptionMail extends Notification
 {
     use Queueable;
 
-    public $content, $user;
+    public $content, $user, $userData = [];
 
     public function __construct($content)
     {
         $this->content = $content;
-        $this->user = auth()->check() ? auth()->user()->getAttribute('fullname') . ' (#' . auth()->id() . ')' : 'Guest';
+        $this->userData['fullname'] = auth()->check() ? auth()->user()->getAttribute('fullname') . ' (#' . auth()->id() . ')' : 'Guest';
+        $this->userData['ip'] = request()->ip();
+        $this->userData['device'] = request()->header('User-Agent');
     }
 
     public function via($notifiable)
@@ -32,7 +35,9 @@ class ExceptionMail extends Notification
                     ->line(new HtmlString('<strong>Message: </strong>' . $this->content->getMessage()))
                     ->line(new HtmlString('<strong>Error file: </strong>' . $this->content->getFile()))
                     ->line(new HtmlString('<strong>Error line: </strong>' . $this->content->getLine()))
-                    ->line(new HtmlString('<strong>User: </strong>' . $this->user))
+                    ->line(new HtmlString('<strong>User: </strong>' . $this->userData['fullname']))
+                    ->line(new HtmlString('<strong>IP: </strong>' . $this->userData['ip']))
+                    ->line(new HtmlString('<strong>Device: </strong>' . $this->userData['device']))
                     ->line(new HtmlString('<strong>Error Traces: </strong></br><pre>' . print_r(collect($this->content->getTrace())->pluck('class')->toArray(), true) . '</pre>'));
     }
 }
