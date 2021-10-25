add71716798b4c2df3bc45e5c92753406fa1d136
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-10-25 22:54:43 +0400

    timeline edited

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,30 +1,28 @@
-<link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
-<div id="timeline-container">
-    <div class="container">
-    <div class="item">
-        <div id="timeline">
-            <div>
-{{--                @dd($updates)--}}
-{{--                @foreach($updates as $date => $update)--}}
-{{--                    @dd(\Carbon\Carbon::parse($date)->format('Y F d'))--}}
-{{--                @endforeach--}}
-                @foreach($updates as $date => $update)
-                    <section class="year">
-                        <h3>{{\Carbon\Carbon::parse($date)->format('Y F d')}}</h3>
-                        @foreach($update as $subUpdate)
-                            <section>
-                                <ul>
-                                    <li style="font-weight: 900;font-size: 23px">{{$subUpdate->name}}</li>
-                                    @foreach($subUpdate->updates as $subUpdates)
-                                        <li>{{$subUpdates->name}}</li>
-                                    @endforeach
-                                </ul>
+@extends('layouts.main')
+@section('content')
+    <div id="timeline-container">
+        <div class="container">
+            <div class="item">
+                <div id="timeline">
+                    <div>
+                        @foreach($updates as $date => $update)
+                            <section class="year">
+                                <h3>{{\Carbon\Carbon::parse($date)->format('Y F d')}}</h3>
+                                @foreach($update as $subUpdate)
+                                    <section>
+                                        <ul>
+                                            <li style="font-weight: 900;font-size: 23px">{{$subUpdate->name}}</li>
+                                            @foreach($subUpdate->updates as $subUpdates)
+                                                <li>{{$subUpdates->name}}</li>
+                                            @endforeach
+                                        </ul>
+                                    </section>
+                                @endforeach
                             </section>
                         @endforeach
-                    </section>
-                @endforeach
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
-</div>
+@endsection
