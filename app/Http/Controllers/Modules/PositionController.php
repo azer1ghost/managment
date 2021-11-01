1beaff2aecaae9fa1e70608bfef92eb56ecca2b2
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-11-01 12:06:10 +0400

    positions paginate bug fixed

--- app/Http/Controllers/Modules/PositionController.php
+++ app/Http/Controllers/Modules/PositionController.php
@@ -27,7 +27,7 @@ class PositionController extends Controller
                     ->when($search, fn($q) => $q->where('name', 'like', "%{$search}%"))
                     ->when($role, fn($q) => $q->where('role_id', $role))
                     ->when($department, fn($q) => $q->where('department_id', $department))
-                    ->orderBy('order', 'asc')
+                    ->orderBy('order')
                     ->paginate(10),
                 'roles' => Role::get(['id', 'name']),
                 'departments' => Department::get(['id', 'name']),
