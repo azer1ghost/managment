6d8eb36b12c2e99b7c37a6af8d5b0da7e40cf49a
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-11-02 17:34:13 +0400

    documents bug fixed

--- resources/views/panel/pages/inquiry/edit.blade.php
+++ resources/views/panel/pages/inquiry/edit.blade.php
@@ -41,7 +41,7 @@
     <div class="mt-4 pt-3" style="clear: both">
         <livewire:inquiry-form :action="$action"  :method="$method" :inquiry="$data" />
     </div>
-    @if(!is_null($data))
+    @if(!is_null($data) && $data->getAttribute('id') != null)
         <x-documents :documents="$data->documents"/>
         <x-document-upload :id="$data->id" model="Inquiry"/>
     @endif
