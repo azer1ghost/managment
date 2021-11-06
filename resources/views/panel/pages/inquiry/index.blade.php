9def91645b25023e473321e7b6e41c0e1d73eba8
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-11-06 13:57:30 +0400

    summernote problems fixed

--- resources/views/panel/pages/inquiry/index.blade.php
+++ resources/views/panel/pages/inquiry/index.blade.php
@@ -371,29 +371,27 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
     <script>
+        const subject = $('#subjectFilter').val().join(',');
+        const company = $('#companyFilter').val().join(',');
+        const status = $('#statusFilter').val().join(',');
+        const contact_method = $('#contactMethodFilter').val().join(',');
+        const source = $('#sourceFilter').val().join(',');
+
+        const inquiryRoute = '{{route('inquiry.index')}}';
+        const params = new URLSearchParams({
+            subject,
+            company,
+            status,
+            contact_method,
+            source,
+        });
 
         $('select[name="limit"]').change(function () {
-            this.form.submit();
+            window.location = inquiryRoute + "?" + $(this).serialize() + "&" + params.toString();
         });
 
         $('#inquiryForm').on('submit', function (e) {
             e.preventDefault();
-
-            const subject = $('#subjectFilter').val().join(',');
-            const company = $('#companyFilter').val().join(',');
-            const status = $('#statusFilter').val().join(',');
-            const contact_method = $('#contactMethodFilter').val().join(',');
-            const source = $('#sourceFilter').val().join(',');
-
-            const inquiryRoute = '{{route('inquiry.index')}}';
-            const params = new URLSearchParams({
-                subject,
-                company,
-                status,
-                contact_method,
-                source,
-            });
-
             window.location = inquiryRoute + "?" + $(this).serialize() + "&" + params.toString();
         });
 
