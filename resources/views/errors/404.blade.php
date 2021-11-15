1e852ead80be425cd891fef1796015abead6ad4e
Valiyev <aliveliyev016@gmail.com>
2021-11-15 09:11:06 +0400

    403 & 404 page done

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,8 +6,33 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+       body{
+           background-color: black;
+       }
+       div{
+           color: white;
+           text-align: center;
+
+       }
+       .container{
+           font-size: 25px;
+
+           padding: 30vh;
+       }
+
+
+
+    </style>
 </head>
 <body>
+    <div>
+        <div class="container">
+
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        </div>
+    </div>
 
 </body>
 </html>
