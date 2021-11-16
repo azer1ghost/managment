82251253e1d0a046b3a84cb14446a109d880eee1
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-11-16 17:45:19 +0400

    file viewer front problem fixed

--- resources/views/panel/pages/main/file-viewer.blade.php
+++ resources/views/panel/pages/main/file-viewer.blade.php
@@ -12,18 +12,19 @@
 
 @section('content')
     <div class="row m-0" style="position: relative">
-        @php($images = ['image/jpeg', 'image/jpg', 'image/png'])
-        @if(in_array($document->type, $images))
-            <img src="{{route('document.temporaryUrl', $document)}}" alt="img" style="width: 500px">
-        @else
-            <iframe
-                    src="https://view.officeapps.live.com/op/embed.aspx?src={{route('document.temporaryUrl', $document)}}"
-                    style="width:100%;height:600px;border: 0"
-            >
-                <p>Your browser does not support iframes.</p>
-            </iframe>
-        @endif
-        <br/>
+        <div>
+            @php($images = ['image/jpeg', 'image/jpg', 'image/png'])
+            @if(in_array($document->type, $images))
+                <img src="{{route('document.temporaryUrl', $document)}}" alt="img" style="width: 500px">
+            @else
+                <iframe
+                        src="https://view.officeapps.live.com/op/embed.aspx?src={{route('document.temporaryUrl', $document)}}"
+                        style="width:100%;height:600px;border: 0"
+                >
+                    <p>Your browser does not support iframes.</p>
+                </iframe>
+            @endif
+        </div>
         <a href="{{route('document.temporaryUrl', $document)}}" download class="btn btn-outline-primary mt-3"><i class="fa fa-download"></i> Download</a>
     </div>
 @endsection
