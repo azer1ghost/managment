207e88f534a3f8ef9ecc82e3c5813e16a7cd6978
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-11-25 10:05:14 +0400

    Favicon added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,24 +3,21 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
-
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/jpg" href="{{asset('assets/images/diamond-green.png')}}"/>
+    <!-- Google Fonts API -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <!-- Flags Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Styles -->
     <link href="{{ asset('assets/fonts/fontawesome.pro.min.css') }}" rel="stylesheet">
     <link href="{{ mix('assets/css/app.css') }}" rel="stylesheet">
-
     @yield('style')
     @stack('style')
-
     @livewireStyles
 </head>
 <body class="custom-scrollbar">
