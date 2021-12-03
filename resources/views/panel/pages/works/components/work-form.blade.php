a03d767d254a8e8a67470d58bde55a3182a56d03
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-03 11:18:11 +0400

    work form datetime bug fixed

--- resources/views/panel/pages/works/components/work-form.blade.php
+++ resources/views/panel/pages/works/components/work-form.blade.php
@@ -135,7 +135,7 @@
                 @endif
 
                 @if(!is_null($data) && !is_null(optional($data)->getAttribute('datetime')))
-                    <x-input::text wire:ignore name="datetime" :label="__('translates.fields.date')" value="{{optional($data->getAttribute('datetime'))->format('Y-m-d H:i') ?? now()->format('Y-m-d H:i')}}" type="text" width="3" class="pr-3" />
+                    <x-input::text wire:ignore name="datetime" :label="__('translates.fields.date')" value="{{$data->getAttribute('datetime')->format('Y-m-d H:i')}}" type="text" width="3" class="pr-3" />
                 @endif
 
                 @foreach($parameters as $parameter)
