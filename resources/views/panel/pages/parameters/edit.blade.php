48cd03a65f4822acdfc81b3e310827ac88ce3c53
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-07 15:49:35 +0400

    department company parameter done

--- resources/views/panel/pages/parameters/edit.blade.php
+++ resources/views/panel/pages/parameters/edit.blade.php
@@ -99,7 +99,7 @@
                         <br/>
                         @forelse ($department->departmentCompanies()->where('parameter_id', optional($data)->getAttribute('id'))->get() as $departmentCompany)
                             <label for="optionFilter-{{$department->getAttribute('id')}}-{{$departmentCompany->getAttribute('id')}}">{{$departmentCompany->getAttribute('name')}}</label>
-                            <select name="options[{{$department->getAttribute('id')}}][{{$departmentCompany->getAttribute('id')}}][]" data-selected-text-format="count" id="optionFilter-{{$department->getAttribute('id'}}-{{$departmentCompany->getAttribute('id')}}" multiple class="filterSelector" data-width="fit"  title="Noting selected" >
+                            <select name="options[{{$department->getAttribute('id')}}][{{$departmentCompany->getAttribute('id')}}][]" data-selected-text-format="count" id="optionFilter-{{$department->getAttribute('id')}}-{{$departmentCompany->getAttribute('id')}}" multiple class="filterSelector" data-width="fit"  title="Noting selected" >
                                 @foreach ($options as $option)
                                     <option @if($department->options()->where('parameter_id', optional($data)->getAttribute('id'))->get()->contains($option->getAttribute('id'))) selected  @endif
                                         value="{{$option->getAttribute('id')}}">
