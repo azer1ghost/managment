098c7e5cc5117c7081fe7e92382e0b90bf6e4c45
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-10 11:33:46 +0400

    announcements done

--- resources/views/components/notify-modal.blade.php
+++ resources/views/components/notify-modal.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" tabindex="-1" id="notify-modal" data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-centered {{$announcement->getAttribute('class')}}">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">{{$announcement->getAttribute('title')}}</h5>
