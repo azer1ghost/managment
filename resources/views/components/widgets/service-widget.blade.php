a3c83f80bb65fe4c2ea4eb5bcc8ecabd706a343a
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-21 10:36:39 +0400

    services widget done

--- resources/views/components/widgets/service-widget.blade.php
+++ resources/views/components/widgets/service-widget.blade.php
@@ -1,7 +1,9 @@
 <div class="{{$widget->class_attribute}}">
     <div class="card border-0" style="background: #e9ecef !important;">
         <div class="py-2 px-1">
-            <div id="{{$widget->key}}" style="width: 100%;{{$widget->style_attribute}}"></div>
+            <div id="{{$widget->key}}" style="width: 100%;{{$widget->style_attribute}}" class="text-center">
+                <h3>{{$widget->details}}</h3>
+            </div>
             <script>
                 am5.ready(function() {
                     const {{$model}}Root = am5.Root.new("{{$widget->key}}");
@@ -31,7 +33,7 @@
                     {{$model}}Series.slices.template.set("strokeOpacity", 1);
 
                     const {{$model}}Legend = {{$model}}Chart.children.push(am5.Legend.new({{$model}}Root, {
-                        layout: {{$model}}Root.horizontalLayout,
+                        layout: {{$model}}Root.verticalLayout,
                         y: am5.percent(50),
                         centerY: am5.percent(50),
                         maxColumns: 1,
@@ -44,6 +46,7 @@
                         cornerRadiusBR: 10
                     });
                     {{$model}}Legend.data.setAll({{$model}}Series.dataItems);
+
                     {{$model}}Series.appear(1000, 100);
                 });
             </script>
