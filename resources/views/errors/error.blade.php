3efbb4d4013bf40f66e640c4354362a52dcbd7f7
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-25 13:02:36 +0400

    error layout upgraded

--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{config('app.name')}} Coming Soon</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
