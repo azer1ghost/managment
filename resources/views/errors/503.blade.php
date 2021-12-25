daca91b7074046d65e7ccca5cc3f9f88cf7424fb
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-25 13:16:39 +0400

    error layout upgraded

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,6 +1,13 @@
 @extends('errors.error')
 @section('error', '503')
-@section('content', 'Hal-hazırda Sistemdə Profilaktik İşlər Gedir.Anlayışınız Üçün Təşəkkürlər')
+@section('content', 'Hal-hazırda Sistemdə Profilaktik İşlər Gedir. Anlayışınız Üçün Təşəkkürlər.')
 @section('downtime')
-    {{\Carbon\Carbon::parse(config('app.downtime'))->locale('az')->diffForHumans()}}
+    @php($down_time = \Carbon\Carbon::parse('31.12.2021 23:59'))
+    {{$down_time->locale('az')->diffForHumans(['parts' => 2, 'short' => false])}} yenidən yoxlayın
+
+    <script>
+        setTimeout(function (){
+            window.location.reload();
+        }, 2000);
+    </script>
 @endsection
