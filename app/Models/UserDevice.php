9db7f0e856e632384e212484e05a0f7f49d14eda
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-27 15:52:10 +0400

    accountant logger added

--- app/Models/UserDevice.php
+++ app/Models/UserDevice.php
@@ -2,15 +2,11 @@
 
 namespace App\Models;
 
-use Altek\Accountant\Contracts\Recordable;
-use Altek\Eventually\Eventually;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserDevice extends Model implements Recordable
+class UserDevice extends Model
 {
-    use \Altek\Accountant\Recordable, Eventually;
-
     protected $primaryKey = 'device_key';
     protected $fillable = ['user_id', 'device', 'device_key', 'fcm_token', 'ip', 'location'];
 
