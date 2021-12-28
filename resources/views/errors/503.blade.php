355ed82bb3aaa99d4e432d37ef892a81919607a1
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-28 20:22:43 +0400

    inquiry status widget upgraded

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,7 +2,7 @@
 @section('error', '503')
 @section('content', 'Hal-hazırda Sistemdə Profilaktik İşlər Gedir. Anlayışınız Üçün Təşəkkürlər.')
 @section('downtime')
-    @php($down_time = \Carbon\Carbon::parse('27.12.2021 16:00'))
+    @php($down_time = \Carbon\Carbon::parse('28.12.2021 23:59'))
     {{$down_time->locale('az')->diffForHumans(['parts' => 2, 'short' => false])}} yenidən yoxlayın
 
     <script>
