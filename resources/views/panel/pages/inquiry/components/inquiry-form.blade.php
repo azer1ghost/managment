e113e71ca3ba19dcba583ed36d546a0b81f72153
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-12-28 21:34:24 +0400

    inquiry status widget upgraded

--- resources/views/panel/pages/inquiry/components/inquiry-form.blade.php
+++ resources/views/panel/pages/inquiry/components/inquiry-form.blade.php
@@ -26,15 +26,17 @@
 
     <input type="hidden" name="company_id" wire:model="selected.company">
 
-    <div class="form-group col-12 col-md-3 mb-3">
-        <label class="d-block" for="clientFilter">{{trans('translates.general.select_client')}}</label>
-        <select id="clientFilter" class="client-filter" style="width: 100% !important;" disabled>
-            @if(is_numeric($client))
-                <option value="{{$client}}">{{\App\Models\Client::find($client)->getAttribute('fullname_with_voen')}}</option>
-            @endif
-        </select>
-        <input type="hidden" name="client_id" value="{{$client}}">
-    </div>
+    @if(auth()->user()->getAttribute('department_id') == \App\Models\Department::SALES)
+        <div class="form-group col-12 col-md-3 mb-3">
+            <label class="d-block" for="clientFilter">{{trans('translates.general.select_client')}}</label>
+            <select id="clientFilter" class="client-filter" style="width: 100% !important;" disabled>
+                @if(is_numeric($client))
+                    <option value="{{$client}}">{{\App\Models\Client::find($client)->getAttribute('fullname_with_voen')}}</option>
+                @endif
+            </select>
+            <input type="hidden" name="client_id" value="{{$client}}">
+        </div>
+    @endif
 
     @foreach($formFields as $formField)
         @if($formField['type'] === 'select' && count($formField['options']) == 0)
