0d52f0ab463a9834d32883af29691f1ceea2bae6
Gafar <qafarzade2014@gmail.com>
2022-01-12 15:27:59 +0400

    Sales Client delete

--- app/Policies/SalesClientPolicy.php
+++ app/Policies/SalesClientPolicy.php
@@ -34,6 +34,8 @@ class SalesClientPolicy
 
     public function delete(User $user, SalesClient $salesClient): bool
     {
-        return $this->canManage($user, 'salesClient', __FUNCTION__ ) || ($salesClient->getAttribute('user_id') == $user->getAttribute('id') && $salesClient->getAttribute('created_at')->diff(now())->h < 24);
+        return
+            $this->canManage($user, 'salesClient', __FUNCTION__ ) ||
+            ($salesClient->getAttribute('user_id') == $user->getAttribute('id') && $salesClient->getAttribute('created_at')->diff(now())->H < 24);
     }
 }
