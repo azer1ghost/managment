86fdbee1b5da31ef35ac984a1e5fb08bfef07cc2
Gafar <qafarzade2014@gmail.com>
2022-01-31 16:54:59 +0400

    customer engagement

--- database/migrations/2021_11_27_123734_create_customer_engagements_table.php
+++ database/migrations/2021_11_27_123734_create_customer_engagements_table.php
@@ -15,7 +15,7 @@ class CreateCustomerEngagementsTable extends Migration
     {
         Schema::create('customer_engagements', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->index()->constrained()->onDelete('CASCADE');
+            $table->foreignId('user_id')->nullable()->index()->constrained()->onDelete('CASCADE');
             $table->foreignId('client_id')->index()->constrained()->onDelete('CASCADE');
             $table->integer('partner_id')->nullable();
             $table->softDeletes();
