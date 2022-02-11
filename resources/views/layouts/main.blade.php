64c332533bb8a004cb73679074b437b97168ff2a
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2022-02-11 14:26:04 +0400

    refactoring new template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Robots -->
+    <meta name="robots" content="nofollow,noindex">
     <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
     <!-- Favicons -->
     <link rel="icon" type="image/x-icon" href="{{asset('assets/images/favicons/favicon.ico')}}"/>
