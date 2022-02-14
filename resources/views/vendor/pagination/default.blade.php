20858a066b663148184b753f52bea52601e9683d
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2022-02-14 23:18:56 +0400

    refactoring new template

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -11,25 +11,26 @@
 
             {{-- Pagination Elements --}}
             @foreach ($elements as $element)
-            <!-- Array Of Links -->
-                @foreach ($element as $page => $url)
-                <!--  Use three dots when current page is greater than 4.  -->
-                    @if ($paginator->currentPage() > 4 && $page === 2)
-                        <li class="page-item disabled"><span class="page-link">...</span></li>
-                    @endif
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                    <!--  Use three dots when current page is greater than 4.  -->
+                        @if ($paginator->currentPage() > 4 && $page === 2)
+                            <li class="page-item disabled"><span class="page-link">...</span></li>
+                        @endif
 
-                <!--  Show active page else show the first and last two pages from current page.  -->
-                    @if ($page == $paginator->currentPage())
-                        <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
-                    @elseif ($page === $paginator->currentPage() + 1 || $page === $paginator->currentPage() + 2 || $page === $paginator->currentPage() - 1 || $page === $paginator->currentPage() - 2 || $page === $paginator->lastPage() || $page === 1)
-                        <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
-                    @endif
+                    <!--  Show active page else show the first and last two pages from current page.  -->
+                        @if ($page == $paginator->currentPage())
+                            <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
+                        @elseif ($page === $paginator->currentPage() + 1 || $page === $paginator->currentPage() + 2 || $page === $paginator->currentPage() - 1 || $page === $paginator->currentPage() - 2 || $page === $paginator->lastPage() || $page === 1)
+                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
 
-                <!--  Use three dots when current page is away from end.  -->
-                    @if ($paginator->currentPage() < $paginator->lastPage() - 3 && $page === $paginator->lastPage() - 1)
-                        <li class="page-item disabled"><span class="page-link">...</span></li>
-                    @endif
-                @endforeach
+                    <!--  Use three dots when current page is away from end.  -->
+                        @if ($paginator->currentPage() < $paginator->lastPage() - 3 && $page === $paginator->lastPage() - 1)
+                            <li class="page-item disabled"><span class="page-link">...</span></li>
+                        @endif
+                    @endforeach
+                @endif
             @endforeach
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
