9db9f826f055bfcca2831901032431e5a9d0e33d
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2022-03-01 16:26:30 +0400

    inquiry user widget fix

--- resources/views/components/widgets/inquiryUser-widget.blade.php
+++ resources/views/components/widgets/inquiryUser-widget.blade.php
@@ -9,7 +9,7 @@
                             <p class="mb-0">{{$user->getAttribute('fullname')}}</p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="progress progress-md flex-grow-1 mr-4">
-                                    <div class="progress-bar {{$colors[$loop->iteration]}}" role="progressbar" style="width: {{($user->getAttribute('inquiries_count')/\App\Models\Inquiry::count())*100}}%; color: red" aria-valuenow="48" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="progress-bar" role="progressbar" style="width: {{round(($user->getAttribute('inquiries_count')/$users->sum('inquiries_count'))*100, 2)}}%; background-color: {{rand_color()}}!important;"></div>
                                 </div>
                                 <p class="mb-0">{{$user->getAttribute('inquiries_count')}}</p>
                             </div>
