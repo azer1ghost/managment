0dbf411811edfd0ba4fffb4a2e4524077419f5a5
Gafar <qafarzade2014@gmail.com>
2022-05-07 11:13:12 +0400

    server error

--- app/Http/Controllers/Modules/WorkController.php
+++ app/Http/Controllers/Modules/WorkController.php
@@ -64,10 +64,10 @@ class WorkController extends Controller
             'created_at' => $request->has('check-created_at'),
         ];
 
-        $usersQuery = User::with('position', 'role')->isActive()->select(['id', 'name', 'surname', 'position_id', 'role_id']);
-        $users = Work::userCannotViewAll() && Work::userCanViewDepartmentWorks() ?
-            $usersQuery->where('department_id', $user->getAttribute('department_id'))->get() :
-            $usersQuery->get();
+//        $usersQuery = User::with('position', 'role')->isActive()->select(['id', 'name', 'surname', 'position_id', 'role_id']);
+//        $users = Work::userCannotViewAll() && Work::userCanViewDepartmentWorks() ?
+//            $usersQuery->where('department_id', $user->getAttribute('department_id'))->get() :
+//            $usersQuery->get();
 
         $departments = Department::get(['id', 'name']);
         $companies = Company::query()->has('asanImzalar')->limit(10)->get();
@@ -93,7 +93,7 @@ class WorkController extends Controller
 
         return view('pages.works.index'
             ,
-            compact('works', 'services', 'departments', 'users',
+            compact('works', 'services', 'departments',
             'filters', 'statuses', 'verifies', 'priceVerifies', 'companies', 'allDepartments', 'dateFilters')
         );
     }
