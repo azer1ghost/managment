bd836f7840a7158034e070157ca40673809c93de
Gafar <qafarzade2014@gmail.com>
2022-05-17 20:14:09 +0400

    bugs

--- app/Http/Controllers/Modules/WorkController.php
+++ app/Http/Controllers/Modules/WorkController.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\WorkRequest;
 use App\Interfaces\WorkRepositoryInterface;
 use Carbon\Carbon;
-use App\Models\{Company, Department, Ledger, Service, User, Work};
+use App\Models\{Company, Department, Service, User, Work};
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
@@ -34,10 +34,6 @@ class WorkController extends Controller
 
     public function index(Request $request)
     {
-
-       return Ledger::query()->count();
-
-
         $user = auth()->user();
         $limit  = $request->get('limit', 25);
         $startOfMonth = now()->firstOfMonth()->format('Y/m/d');
