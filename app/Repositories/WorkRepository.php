8ff4d90c447086b0ed69374ec6bbcb5e2c5ae3f2
Gafar <qafarzade2014@gmail.com>
2022-05-17 19:34:26 +0400

    bugs

--- app/Repositories/WorkRepository.php
+++ app/Repositories/WorkRepository.php
@@ -24,16 +24,16 @@ class WorkRepository implements WorkRepositoryInterface {
 //                'client:id,fullname,voen',
 //                'asanImza:id,user_id,company_id'
 //            ])
-            ->when(true, function ($query) use ($user){
+            ->when(Work::userCannotViewAll(), function ($query) use ($user){
                 if($user->hasPermission('viewAllDepartment-work')){
-                    $query->where('department_id', $user->getAttribute('department_id'));
-                }else{
+//                    $query->where('department_id', $user->getAttribute('department_id'));
+                }
+                else{
                     $query
                         ->where(function ($q) use ($user){
                             $q->whereNull('user_id')->where('department_id', $user->getAttribute('department_id'));
                         })
                         ->orWhere('user_id', $user->getAttribute('id'));
-
                 }
             })
             ->where(function($query) use ($filters, $dateRanges, $dateFilters){
