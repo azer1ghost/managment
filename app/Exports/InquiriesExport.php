da79c9e73df22eb3aa53bbe2d90f3ba77007e2c3
Gafar <qafarzade2014@gmail.com>
2022-07-10 14:41:01 +0400

    inquiry phone export

--- app/Exports/InquiriesExport.php
+++ app/Exports/InquiriesExport.php
@@ -7,20 +7,29 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 
-class InquiriesExport implements FromCollection, WithHeadings
+class InquiriesExport implements FromCollection, WithHeadings, WithMapping
+
+
 {
+    public function collection()
+    {
+        return Inquiry::get();
+    }
+
     public function headings(): array
     {
         return [
-            'Name',
-            'Company',
-            'Phone'
+            '#',
+            'Phone',
         ];
     }
 
-
-    public function collection()
+    public function map($row): array
     {
-        return Inquiry::all();
+        return [
+            $row->id,
+            optional($row->getParameter('phone'))->getAttribute('value'),
+
+        ];
     }
 }
