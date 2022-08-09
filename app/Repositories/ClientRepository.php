2fc36a70d380c984660de02e627710b432b00fba
Valiyev <aliveliyev016@gmail.com>
2022-08-09 11:44:43 +0400

    search

--- app/Repositories/ClientRepository.php
+++ app/Repositories/ClientRepository.php
@@ -27,6 +27,9 @@ class ClientRepository implements ClientRepositoryInterface {
             ->when(is_numeric($filters['type']), fn ($query) => $query->where('type', (int) $filters['type']))
             ->when(is_numeric($filters['satisfaction']), fn ($query) => $query->where('satisfaction', (int) $filters['satisfaction']))
             ->when($filters['search'], fn ($query) => $query->where('fullname', 'like', "%{$filters['search']}%"))
+                                                            ->orWhere('voen', 'like' , "%{$filters['search']}%")
+                                                            ->orWhere('phone1', 'like', "%{$filters['search']}%")
+                                                            ->orWhere('phone2', 'like', "%{$filters['search']}%")
             ->when($filters['salesClient'], fn ($query) => $query->whereHas('salesUsers', fn($q) => $q->where('id', $filters['salesClient'])));
     }
 }
