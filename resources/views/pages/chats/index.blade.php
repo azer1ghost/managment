983f27cb9da08d7ca1072721d78abe6535a8a017
Gafar <qafarzade2014@gmail.com>
2022-08-26 13:54:00 +0400

    chat photo

--- resources/views/pages/chats/index.blade.php
+++ resources/views/pages/chats/index.blade.php
@@ -27,7 +27,7 @@
                             <li class="removing-list" >Mesaj Göndərilənlər</li>
                             @foreach($recentUsers as $user)
                                 <li class="clearfix user removing-list" id="{{$user->getAttribute('id')}}">
-                                    <img src="{{image($user->getAttribute('avatar'))}}" loading="lazy" alt="avatar" class="profile">
+{{--                                    <img src="{{image($user->getAttribute('avatar'))}}" loading="lazy" alt="avatar" class="profile">--}}
                                     <div class="about">
                                         <div class="name">{{$user->getAttribute('fullname')}}</div>
                                         <div>
@@ -44,7 +44,7 @@
                             <li>Bütün İstifadəçilər</li>
                             @foreach($users as $user)
                                 <li class="clearfix user searching-list" id="{{$user->getAttribute('id')}}">
-                                    <img src="{{image($user->getAttribute('avatar'))}}" loading="lazy" alt="avatar" class="profile">
+{{--                                    <img src="{{image($user->getAttribute('avatar'))}}" loading="lazy" alt="avatar" class="profile">--}}
                                     <div class="about">
                                         <div class="name">{{$user->getAttribute('fullname')}}</div>
                                         <div>
