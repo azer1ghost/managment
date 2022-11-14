9cdab54d0b4e73ca2b1aa0a2936115c46f4e519e
Gafar <qafarzade2014@gmail.com>
2022-11-14 16:59:52 +0400

    chat changes

--- resources/views/pages/internal_numbers/index.blade.php
+++ resources/views/pages/internal_numbers/index.blade.php
@@ -66,7 +66,6 @@
                                     @endcan
                                 </div>
                             </td>
-                            </td>
                         </tr>
                     @empty
                         <tr>
