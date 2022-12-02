ee8d24b7f9b0c2b9a309b8b61f5b2576625f2cec
Gafar <qafarzade2014@gmail.com>
2022-12-02 11:24:39 +0400

    structure

--- resources/views/pages/instructions/structure.blade.php
+++ resources/views/pages/instructions/structure.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('title', trans('translates.navbar.structure'))
 @section('content')
-    <img style="width: 100%" src="{{asset('assets/images/dashboard/structure.jpg')}}" alt="">
+    <img style="width: 100%" src="{{asset('assets/images/dashboard/structures.jpg')}}" alt="">
 @endsection
 
