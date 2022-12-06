c50367c0424b53a95f20ed18da112daf85a30cc0
Valiyev <aliveliyev016@gmail.com>
2022-12-06 10:15:51 +0400

    job instruction

--- resources/views/pages/job-instructions/index.blade.php
+++ resources/views/pages/job-instructions/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', trans('translates.navbar.job_instruction'))
 
 @section('content')
-    <style>
+    <style href="https://fonts.googleapis.com/css2?family=Lato&display=swap">
         h2 {
             background: #f5f7ff;
             border: 1px #F5F6FC solid;
@@ -11,6 +11,89 @@
             padding: 5px;
             color: #000000;
             cursor: pointer;
+        }
+        body {
+            background: #e0e5ec;
+        }
+        h1 {
+            position: relative;
+            text-align: center;
+            color: #353535;
+            font-size: 50px;
+            font-family: "Cormorant Garamond", serif;
+        }
+
+        p {
+            font-family: 'Lato', sans-serif;
+            font-weight: 300;
+            text-align: center;
+            font-size: 18px;
+            color: #676767;
+        }
+        .frame {
+            width: 90%;
+            margin: 40px auto;
+            text-align: center;
+        }
+        button {
+            margin: 20px;
+        }
+        .custom-btn {
+            width: auto;
+            height: auto;
+            color: #fff;
+            border-radius: 5px;
+            padding: 10px 25px;
+            font-family: 'Lato', sans-serif;
+            font-weight: 500;
+            background: transparent;
+            cursor: pointer;
+            transition: all 0.3s ease;
+            position: relative;
+            display: inline-block;
+            box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
+            7px 7px 20px 0px rgba(0,0,0,.1),
+            4px 4px 5px 0px rgba(0,0,0,.1);
+            outline: none;
+        }
+        .btn-1 {
+            width: 250px;
+            height: 250px;
+            padding: 50px;
+            border: none;
+            background: rgb(0, 21, 255);
+            background: linear-gradient(0deg, rgb(8, 0, 255) 0%, rgb(2, 52, 251) 100%);
+        }
+        .btn-1:hover {
+            color: #0915f0;
+            background: transparent;
+            box-shadow:none;
+        }
+        .btn-1:before,
+        .btn-1:after{
+            content:'';
+            position:absolute;
+            top:0;
+            right:0;
+            height:2px;
+            width:0;
+            background: #0915f0;
+            box-shadow:
+                    -1px -1px 5px 0px #fff,
+                    7px 7px 20px 0px #0003,
+                    4px 4px 5px 0px #0002;
+            transition:400ms ease all;
+        }
+        .btn-1:after{
+            right:inherit;
+            top:inherit;
+            left:0;
+            bottom:0;
+        }
+        .btn-1:hover:before,
+        .btn-1:hover:after{
+            width:100%;
+            transition:800ms ease all;
         }
     </style>
     <x-bread-crumb>
@@ -21,38 +104,62 @@
             @lang('translates.navbar.job_instruction')
         </x-bread-crumb-link>
     </x-bread-crumb>
-    <div class="row mb-2">
-        <div class="col-12 justify-content-center">
-            @foreach($jobInstructions as $jobInstruction)
-                <h2 data-toggle="collapse" href="#instruction-{{$loop->iteration}}">{{$jobInstruction->getRelationValue('users')->getFullnameWithPositionAttribute()}}
-                    <div class="btn-sm-group">
-                        @can('update', App\Models\JobInstruction::class)
-                            <a href="{{route('job-instructions.edit', $jobInstruction)}}" class="btn btn-sm btn-outline-success">
-                                <i class="fal fa-pen"></i>
-                            </a>
-                            <a href="{{route('job-instructions.destroy', $jobInstruction)}}" delete data-name="{{$jobInstruction->getRelationValue('users')->getAttribute('name')}}" class="btn btn-sm btn-outline-danger" >
-                                <i class="fal fa-trash"></i>
-                            </a>
-                        @endcan
-                    </div>
-                </h2>
 
-                <div class="collapse" id="instruction-{{$loop->iteration}}">
-                    <p > {!! $jobInstruction->getAttribute('instruction') !!}</p>
+    @foreach($jobInstructions as $jobInstruction)
+
+        <button type="button" class="custom-btn btn-1" href="#instruction-{{$loop->iteration}}" data-toggle="modal" data-target="#instruction-{{$loop->iteration}}" data-jobInstruction="@json($jobInstruction)">
+            {{$jobInstruction->getRelationValue('users')->getFullnameWithPositionAttribute()}}
+        </button>
+
+    <div class="modal fade bd-example-modal-lg" id="instruction-{{$loop->iteration}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">{{$jobInstruction->getRelationValue('users')->getFullnameWithPositionAttribute()}}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-            @endforeach
-        </div>
 
-        <div class="col-6">
-            <div class="float-right">
-                {{$jobInstructions->appends(request()->input())->links()}}
+                <div class="modal-body">
+                            <p class="text-black">{!! $jobInstruction->getAttribute('instruction') !!}</p>
+                </div>
+
+                <div class="modal-footer">
+                    @can('update', App\Models\JobInstruction::class)
+                        <a href="{{route('job-instructions.edit', $jobInstruction)}}" class="btn btn-sm btn-outline-success">
+                            <i class="fal fa-pen"></i>
+                        </a>
+                        <a href="{{route('job-instructions.destroy', $jobInstruction)}}" delete data-name="{{$jobInstruction->getRelationValue('users')->getAttribute('name')}}" class="btn btn-sm btn-outline-danger" >
+                            <i class="fal fa-trash"></i>
+                        </a>
+                    @endcan
+                </div>
+
             </div>
         </div>
     </div>
+    @endforeach
     @can('create', App\Models\JobInstruction::class)
         <div class="col-12">
             <a class="btn btn-outline-success float-right"
                href="{{route('job-instructions.create')}}">@lang('translates.buttons.create')</a>
         </div>
     @endcan
+    <div class="col-6">
+        <div class="float-right">
+            {{$jobInstructions->appends(request()->input())->links()}}
+        </div>
+    </div>
 @endsection
+
+{{--@section('scripts')--}}
+{{--    <script>--}}
+{{--        $('.custom-btn').click (function (){--}}
+{{--            var jobInstruction = $(this).data('instruction')--}}
+
+{{--            $('.instruction-text').attr(jobInstruction);--}}
+{{--        })--}}
+{{--    </script>--}}
+{{--@endsection--}}
