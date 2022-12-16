fc63318df26c9253d5fa22f1d5073efb2fa417a6
Valiyev <aliveliyev016@gmail.com>
2022-12-16 11:47:18 +0400

    services paginate

--- resources/views/pages/services/index.blade.php
+++ resources/views/pages/services/index.blade.php
@@ -22,13 +22,6 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-2">
-                <select name="limit" class="custom-select">
-                    @foreach([10, 25, 50] as $size)
-                        <option @if(request()->get('limit') == $size) selected @endif value="{{$size}}">{{$size}}</option>
-                    @endforeach
-                </select>
-            </div>
             @can('create', App\Models\Service::class)
                 <div class="col-3">
                     <a class="btn btn-outline-success float-right" href="{{route('services.create')}}">@lang('translates.buttons.create')</a>
@@ -114,11 +107,6 @@
                     @endforelse
                     </tbody>
                 </table>
-            </div>
-            <div class="col-12">
-                <div class="float-right">
-                    {{$services->appends(request()->input())->links()}}
-                </div>
             </div>
         </div>
     </form>
