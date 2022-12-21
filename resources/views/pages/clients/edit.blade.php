f50e3aa70ccc815c9dba234fc079b48e3e080af0
Gafar <qafarzade2014@gmail.com>
2022-12-21 16:32:14 +0400

    clients

--- resources/views/pages/clients/edit.blade.php
+++ resources/views/pages/clients/edit.blade.php
@@ -146,7 +146,7 @@
 
             <div class="form-group col-md-3">
                 <label for="email1">@lang('translates.fields.email1')</label>
-                <input type="email" id="email1" class="form-control" name="email1" value="{{optional($data)->getAttribute('email1')}}" @if(auth()->id() !== 103 || auth()->id() !== 123 || auth()->id() !== 124) required @endif>
+                <input type="email" id="email1" class="form-control" name="email1" value="{{optional($data)->getAttribute('email1')}}" @if(!auth()->user()->hasPermission('satisfactionMeasure-client')) required @endif>
             </div>
 
             <x-form-group  class="pr-3 col-12 col-lg-3" :label="trans('translates.fields.email2')" >
