ee3f2e6287cd0020695c1d07d263fd613f6325cb
Gafar <qafarzade2014@gmail.com>
2023-01-26 16:47:22 +0400

    customer link

--- app/Http/Controllers/Modules/WorkController.php
+++ app/Http/Controllers/Modules/WorkController.php
@@ -190,7 +190,7 @@ class WorkController extends Controller
         $clientName = str_replace($search,$replace,$clientText);
         if (!empty($client->getAttribute('phone1') && $client->getAttribute('send_sms') == 1)) {
                 if ($request->status == 3) {
-                    $message = 'Deyerli ' . $clientName . ' sizin ' . $serviceName . ' uzre isiniz tamamlandi. ' . $work->getAttribute('created_at')->format('d/m/y') . ' https://wf.qmeter.net/LLbJKa -linke kecid ederek xidmeti keyfiyyetini deyerlendirmeyinizi xahis edirik!';
+                    $message = 'Deyerli ' . $clientName . ' sizin ' . $serviceName . ' uzre isiniz tamamlandi. ' . $work->getAttribute('created_at')->format('d/m/y') . ' https://my.mobilgroup.az/cs?url=mb-sat -linke kecid ederek xidmet keyfiyyetini deyerlendirmeyinizi xahis edirik!';
                     (new NotifyClientSms($message))->toSms($client)->send();
                 }
             }
