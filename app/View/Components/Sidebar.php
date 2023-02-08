a9ee8fb57b7a4d440f9835722293126c90836ed3
Valiyev <aliveliyev016@gmail.com>
2023-02-08 14:50:37 +0400

    permission

--- app/View/Components/Sidebar.php
+++ app/View/Components/Sidebar.php
@@ -34,7 +34,7 @@ class Sidebar extends Component
                 'children' => [
                     (object)[
                         'title' => __('translates.navbar.signature'),
-                        'icon' => 'fa fa-envelope',
+                        'icon' => 'fa fa-e nvelope',
                         'url' => route('signature-select-company'),
                         'permission' => 'signature',
                     ],
