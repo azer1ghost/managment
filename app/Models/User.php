0ddfb5df554f315faf8a1f596c7e9ed63f1845f7
Gafar <qafarzade2014@gmail.com>
2023-03-09 10:18:07 +0400

    transit

--- app/Models/User.php
+++ app/Models/User.php
@@ -222,7 +222,7 @@ class User extends Authenticatable implements MustVerifyPhone, Recordable
 
     public function isTransitCustomer(): bool
     {
-        return $this->getAttribute('role_id') == 4;
+        return $this->getAttribute('role_id') == 5;
     }
 
     public function isDepartmentChief(): bool
