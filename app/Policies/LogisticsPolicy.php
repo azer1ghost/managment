90fb60215e9098058dfd99cd4c9c3a63b57fb188
Gafar <qafarzade2014@gmail.com>
2023-03-09 16:24:02 +0400

    policy fix

--- app/Policies/LogisticsPolicy.php
+++ app/Policies/LogisticsPolicy.php
@@ -11,28 +11,18 @@ class LogisticsPolicy
 {
     use HandlesAuthorization, HandlesPolicy, GetClassInfo;
 
-    public function viewAny(User $user): bool
-    {
-        return $this->canManage($user, 'logistics', __FUNCTION__);
-    }
-
-    public function view(User $user): bool
-    {
-        return $this->canManage($user, 'logistics', __FUNCTION__);
-    }
-
     public function create(User $user): bool
     {
-        return $this->canManage($user, 'logistics');
+        return $this->canManage($user, $this->getClassShortName('s'), __FUNCTION__);
     }
 
     public function update(User $user): bool
     {
-        return $this->canManage($user, 'logistics');
+        return $this->canManage($user, $this->getClassShortName('s'), __FUNCTION__);
     }
 
     public function delete(User $user): bool
     {
-        return $this->canManage($user, 'logistics', __FUNCTION__);
+        return $this->canManage($user, $this->getClassShortName('s'), __FUNCTION__);
     }
 }
