4e6dad4d1c9db69ae69822c3bc3232c527e91375
Gafar <qafarzade2014@gmail.com>
2023-03-09 17:11:25 +0400

    policy logistics

--- app/Policies/LogisticsPolicy.php
+++ app/Policies/LogisticsPolicy.php
@@ -11,18 +11,4 @@ class LogisticsPolicy
 {
     use HandlesAuthorization, HandlesPolicy, GetClassInfo;
 
-    public function create(User $user): bool
-    {
-        return $this->canManage($user, $this->getClassShortName('s'), __FUNCTION__);
-    }
-
-    public function update(User $user): bool
-    {
-        return $this->canManage($user, $this->getClassShortName('s'), __FUNCTION__);
-    }
-
-    public function delete(User $user): bool
-    {
-        return $this->canManage($user, $this->getClassShortName('s'), __FUNCTION__);
-    }
 }
