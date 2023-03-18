7a716bc0635e7d7879be738e29e3976d743bde5e
Gafar <qafarzade2014@gmail.com>
2023-03-18 14:04:23 +0400

    date customer

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -21,18 +21,6 @@ class EmailController extends Controller
         $emails = [
 
 
-            'Elchin.tagiyev85@gmail.com',
-            'aykhan.gulmammadov@gmail.com',
-            'r.rahimov@mobilbroker.az',
-            'ZUTECH030@gmail.com',
-            'orxan93_quliyev@mail.ru',
-            'Shahin.Gasimov@socardalgidj.az',
-            'nesirnemetov@gmail.com',
-            'akhalilli@proxima.az',
-            'humaartroom@gmail.com',
-            'eliyevrafael70@gmail.com',
-            'mamta@bk.ru',
-            's.novruzi@gmail.com',
             'sham-a@mail.ru',
             'mubariz.memmedov1990@gmail.com',
             'krzayeva@aris.az',
