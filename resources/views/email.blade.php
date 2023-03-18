f12a1fa959c933a0f9d2879610211efc6114fc7d
Gafar <qafarzade2014@gmail.com>
2023-03-18 13:12:11 +0400

    date customer

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,136 +1,108 @@
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<!-- Free to use, HTML email template designed & built by FullSphere. Learn more about us at www.fullsphere.co.uk -->
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
+
+    <!--[if gte mso 9]>
+    <xml>
+        <o:OfficeDocumentSettings>
+            <o:AllowPNG/>
+            <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+    </xml>
+    <![endif]-->
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="x-apple-disable-message-reformatting">
-    <title>Email templates</title>
-    <!-- bootstrap linked -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap');
+    <!--[if !mso]><!-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--<![endif]-->
 
-        body {
-            font-family: 'Inconsolata', monospace;
-            max-width: 800px;
-            font-size: 18px;
-        }
-        /* don't use this just placed for fixing thigs defaults */
-        /*
-          *{
-            padding: 0px;
-            margin: 0px;
-            border: 0px;
-          }  */
+    <!-- Your title goes here -->
+    <title>Newsletter</title>
+    <!-- End title -->
 
-        #main{
-            /* background-color: #aaaaff; */
-            background-image: url("https://i.stack.imgur.com/SvWWN.png");
-            background-size: cover;
-            background-repeat: no-repeat;
-            height: auto;
-        }
-        .one_col{
-            border: 1px;
-            border-color: white;
-            color: white;
-            position: relative;
-            left: 250px;
-            width: 100%;
-            display: inline;
-            background-color:#fffaaa;
-        }
-        #wrapper{
-            text-align: center;
-            display:block;
-            width:100%;
-            margin: 10px;
-            padding: 0px 30px 0px 40px;
-            position: relative;
-            /* top: 0px; */
 
-        }
-        #socials{
-            position: relative;
-            top: 10px;
-            left: 530px;
-            display: inline;
-            justify-content: center;
-        }
-        #foot{
-            color: white;
-            position: relative;
-            left: 550px;
-        }
-        hr{
-            color: white;
-            position: relative;
-            left: 250px;
-        }
-    </style>
-</head>
 
-<body id="main">
-<!-- wrapper div -->
-<div id="wrapper">
-    <header>
-        <!-- banner -->
-        <div id="banner">
-            <img src="https://raw.githubusercontent.com/mdbootstrap/knowledge-base/main/CSS/responsive-email/img/banner.png" alt="banner_image">
-        </div>
-    </header>
-</div>
-<!-- paragraphs -->
-<div class="one_col">
+<!-- You can change background colour here -->
 
-    <h1>First template by HTML</h1>
+<body>
 
-    <p id="first_para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, porro facere nisi eligendi quaerat enim perferendis molestiae necessitatibus est maiores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aspernatur in laborum. Natus quidem tenetur iure id non error quis exercitationem illum voluptatum est veniam libero, nam unde facilis incidunt?
-    </p>
+<!-- Fallback force center content -->
+<div >
 
-    <p id="sec_para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur velit sed dolor commodi qui nulla sit, deleniti, doloribus laboriosam nemo nobis harum? Laboriosam voluptatibus id nostrum pariatur ipsam! Eum blanditiis sint, dolorum perspiciatis similique autem? Quaerat ducimus non natus eligendi.
-    </p>
-    <!-- <button type="button" class="btn btn-light">Learn More</button> -->
-    <a href="http:google.com" target="_blank" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Learn More</a>
-</div>
+{{--    <table align="center" style="text-align: center; vertical-align: top; width: 600px; max-width: 600px; background-color: #ffffff;" width="600">--}}
+{{--        <tbody>--}}
+{{--        <tr>--}}
+{{--            <td style="width: 596px; vertical-align: top; padding-left: 0; padding-right: 0; padding-top: 15px; padding-bottom: 15px;" width="596">--}}
 
-<!-- facebook logo -->
-<div id="socials">
-    <p id="social_logo">
+{{--               <img style=" text-align: center; color: #ffffff;" alt="Logo" src="{{asset('assets/images/logos/broker.png')}}" align="center" >--}}
 
-        <a href="#" target="_blank"><img src="https://raw.githubusercontent.com/mdbootstrap/knowledge-base/main/CSS/responsive-email/img/fb-bw.png"></a>
+{{--            </td>--}}
+{{--        </tr>--}}
+{{--        </tbody>--}}
+{{--    </table>--}}
 
-        <!-- twitter -->
+    <img style="text-align: center;" alt="Image" src="https://i.ibb.co/7WVYNb4/fff.png" >
 
-        <a href="#" target="_blank"><img src="https://raw.githubusercontent.com/mdbootstrap/knowledge-base/main/CSS/responsive-email/img/tw-bw.png"></a>
+{{--    <table align="center" style="text-align: center; vertical-align: top; width: 600px; max-width: 600px; background-color: #ffffff;" width="600">--}}
+{{--        <tbody>--}}
+{{--        <tr>--}}
+{{--            <td style="width: 596px; vertical-align: top; padding-left: 30px; padding-right: 30px; padding-top: 30px; padding-bottom: 40px;" width="596">--}}
 
-        <!-- youtube -->
+{{--                <h1 style="font-size: 20px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 600; text-decoration: none; color: #000000;">Single column, dolor sit amet</h1>--}}
 
-        <a href="#" target="_blank"><img src="https://raw.githubusercontent.com/mdbootstrap/knowledge-base/main/CSS/responsive-email/img/yt-bw.png"></a>
+{{--                <p style="font-size: 15px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: #919293;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mattis ante sed imperdiet euismod. Vivamus fermentum bibendum turpis, et tempor dui. Sed vitae lectus egestas, finibus purus ac, rutrum mauris.</p>--}}
 
-    </p>
+{{--                <p style="font-size: 15px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: #919293;">You can download this template <a target="_blank" style="text-decoration: underline; color: #000000;" href="https://fullsphere.co.uk/misc/free-template/html-email-template.zip" download="HTML Email Template"><u>here</u></a></p>--}}
+
+{{--                <!-- Start button (You can change the background colour by the hex code below) -->--}}
+{{--                <a href="#" target="_blank" style="background-color: #000000; font-size: 15px; line-height: 22px; font-family: 'Helvetica', Arial, sans-serif; font-weight: normal; text-decoration: none; padding: 12px 15px; color: #ffffff; border-radius: 5px; display: inline-block; mso-padding-alt: 0;">--}}
+{{--                    <!--[if mso]>--}}
+{{--                        <i style="letter-spacing: 25px; mso-font-width: -100%; mso-text-raise: 30pt;">&nbsp;</i>--}}
+{{--                      <![endif]-->--}}
+
+{{--                    <span style="mso-text-raise: 15pt; color: #ffffff;">Learn more</span>--}}
+{{--                    <!--[if mso]>--}}
+{{--                        <i style="letter-spacing: 25px; mso-font-width: -100%;">&nbsp;</i>--}}
+{{--                      <![endif]-->--}}
+{{--                </a>--}}
+{{--                <!-- End button here -->--}}
+
+{{--            </td>--}}
+{{--        </tr>--}}
+{{--        </tbody>--}}
+{{--    </table>--}}
+
+{{--    <table align="center" style="text-align: center; vertical-align: top; width: 600px; max-width: 600px; background-color: #000000;" width="600">--}}
+{{--        <tbody>--}}
+{{--        <tr>--}}
+{{--            <td style="width: 596px; vertical-align: top; padding-left: 30px; padding-right: 30px; padding-top: 30px; padding-bottom: 30px;" width="596">--}}
+
+{{--                <!-- Your inverted logo is here -->--}}
+{{--                <img style="width: 180px; max-width: 180px; height: 85px; max-height: 85px; text-align: center; color: #ffffff;" alt="Logo" src="https://fullsphere.co.uk/misc/free-template/images/logo-black-background.jpg" align="center" width="180" height="85">--}}
+
+{{--                <p style="font-size: 13px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: #ffffff;">--}}
+{{--                    Address line 1, London, L2 4LN--}}
+{{--                </p>--}}
+
+{{--                <p style="margin-bottom: 0; font-size: 13px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: #ffffff;">--}}
+{{--                    <a target="_blank" style="text-decoration: underline; color: #ffffff;" href="https://fullsphere.co.uk">--}}
+{{--                        www.fullsphere.co.uk--}}
+{{--                    </a>--}}
+{{--                </p>--}}
+
+{{--            </td>--}}
+{{--        </tr>--}}
+{{--        </tbody>--}}
+{{--    </table>--}}
+
 
 </div>
-<hr>
-<footer style="font-size: 14px;">
-    <p id="contacts">
 
-    <p id="foot"> <cite>created by</cite> @RDtemplates</p>
-    <p id="foot">
-        <email>rdranbir1999@gmail.com</email>
-    </p>
-    <p id="foot">
-        <phone>7439102985</phone>
-    </p>
-    <p id="foot"><cite>&copy; All Copyrights 2022 reserved</cite></p>
-
-    </p>
-</footer>
-
-<div id="scroll">
-
-</div>
 </body>
 
 </html>
