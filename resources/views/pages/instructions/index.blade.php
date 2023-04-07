9d0575eb498d76b383b2d879fda6b5b69c7d5993
Valiyev <aliveliyev016@gmail.com>
2023-04-07 10:58:17 +0400

    video

--- resources/views/pages/instructions/index.blade.php
+++ resources/views/pages/instructions/index.blade.php
@@ -23,12 +23,13 @@
     </style>
     <div class="row mb-2">
         <div class="col-12 justify-content-center">
-            <h2 data-toggle="collapse" href="#task">MB-TL-M002 Tapşırıqlar</h2>
+            <h2 data-toggle="collapse" href="#task">Tapşırıqlar</h2>
             <div class="col-12 collapse" id="task">
+                <p class="text-center">MB-TL-M002 Tapşırıqlar</p>
                 <video class="mr-5" width="100%" controls><source src="{{asset('videos/task.mp4')}}" type="video/mp4"></video>
             </div>
             @if(auth()->user()->isDirector() || auth()->user()->hasPermission('update-user'))
-                <h2 data-toggle="collapse" href="#hr">MB-TL-M003 HR</h2>
+                <h2 data-toggle="collapse" href="#hr">HR</h2>
                 <div class="collapse" id="hr">
                     <p class="text-center">MB-TL-M001 Giriş</p>
                     <video class="mr-5" width="100%" controls><source src="{{asset('videos/hr.mp4')}}" type="video/mp4"></video>
@@ -40,22 +41,26 @@
              @endif
             <h2 data-toggle="collapse" href="#inquiry">@lang('translates.navbar.inquiry')</h2>
             <div class="collapse" id="inquiry">
+                <p class="text-center">MB-TL-M001 Giriş</p>
                 <video class="mr-5" width="100%" controls><source src="{{asset('videos/inquiry.mp4')}}" type="video/mp4"></video>
             </div>
 
-            <h2 data-toggle="collapse" href="#calendar">MB-TL-M004 Təqvim</h2>
+            <h2 data-toggle="collapse" href="#calendar">Təqvim</h2>
             <div class="collapse" id="calendar">
+                <p class="text-center">MB-TL-M004 Təqvim</p>
                 <video class="mr-5" width="100%" controls><source src="{{asset('videos/teqvim.mp4')}}" type="video/mp4"></video>
             </div>
 
-            <h2 data-toggle="collapse" href="#work">MB-TL-M005 İşlər</h2>
+            <h2 data-toggle="collapse" href="#work">İşlər</h2>
             <div class="collapse" id="work">
+                <p class="text-center">MB-TL-M005 İşlər</p>
                 <video class="mr-5" width="100%" controls><source src="{{asset('videos/isler.mp4')}}" type="video/mp4"></video>
             </div>
 
             @if(auth()->user()->isDirector() || auth()->user()->hasPermission('editTable-work'))
-                <h2 data-toggle="collapse" href="#declaration">MB-TL-B001 Qısa idxal bəyannaməsi</h2>
+                <h2 data-toggle="collapse" href="#declaration">Qısa idxal bəyannaməsi</h2>
                 <div class="collapse" id="declaration">
+                    <p class="text-center">MB-TL-B001 Qısa idxal bəyannaməsi</p>
                     <video class="mr-5" width="100%" controls><source src="{{asset('videos/declaration.mp4')}}" type="video/mp4"></video>
                 </div>
             @endif
