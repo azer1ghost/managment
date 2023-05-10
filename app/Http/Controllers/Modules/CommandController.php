b8ba0b5d9fa68446fb75943ff44194631f91ceb1
Gafar <qafarzade2014@gmail.com>
2023-05-10 17:10:50 +0400

    note bug fix

--- app/Http/Controllers/Modules/CommandController.php
+++ app/Http/Controllers/Modules/CommandController.php
@@ -27,7 +27,7 @@ class CommandController extends Controller
         $commands = Command::when($search, fn($query) => $query
             ->where('content', 'like', "%" . $search . "%"))->when($company, fn($query) => $query
             ->where('company_id', $company))
-            ->orderBy('ordering');
+            ->orderByDesc('ordering');
 
         if(is_numeric($limit)) {
             $commands = $commands->paginate($limit);
