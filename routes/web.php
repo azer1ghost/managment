d86f3bf7ebab92fa710b462ab53e81ffe915c17b
Gafar <qafarzade2014@gmail.com>
2023-05-30 16:18:33 +0400

    invoices delete

--- routes/web.php
+++ routes/web.php
@@ -241,7 +241,7 @@ Route::group([
     Route::post('/createFinanceClient',[FinanceClientController::class, 'createFinanceClient'] )->name('createFinanceClient');
     Route::post('/createFinanceInvoice',[FinanceClientController::class, 'createFinanceInvoice'] )->name('createFinanceInvoice');
     Route::get('/financeInvoice/{invoice}',[FinanceClientController::class, 'financeInvoice'] )->name('financeInvoice');
-//    Route::get('/deleteInvoice/{invoice}',[FinanceClientController::class, 'deleteInvoice'] )->name('deleteInvoice');
+    Route::get('/deleteInvoice/{invoice}',[FinanceClientController::class, 'deleteInvoice'] )->name('deleteInvoice');
     Route::get('/invoices',[FinanceClientController::class, 'invoices'] )->name('invoices');
     Route::view('/instruction','pages.instructions.index' )->name('instruction');
     Route::view('/presentations','pages.instructions.presentations' )->name('presentations');
