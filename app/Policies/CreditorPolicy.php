72cc43a2c7d99055d4aed179f5e92f4316f23148
Gafar <qafarzade2014@gmail.com>
2023-06-19 09:42:57 +0400

    finance

--- app/Policies/CreditorPolicy.php
+++ app/Policies/CreditorPolicy.php
@@ -10,5 +10,18 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class CreditorPolicy
 {
     use HandlesAuthorization, HandlesPolicy, GetClassInfo;
+    public function create(User $user): bool
+    {
+        return $this->canManage($user, $this->getClassShortName('s'), __FUNCTION__);
+    }
 
+    public function update(User $user): bool
+    {
+        return $this->canManage($user, $this->getClassShortName('s'), __FUNCTION__);
+    }
+
+    public function delete(User $user): bool
+    {
+        return $this->canManage($user, $this->getClassShortName('s'), __FUNCTION__);
+    }
 }
