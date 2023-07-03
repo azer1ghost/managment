39ca20cc09ff3129f59d82ce35d3f628f3266abb
Valiyev <aliveliyev016@gmail.com>
2023-07-03 17:40:25 +0400

    work

--- resources/views/pages/works/total.blade.php
+++ resources/views/pages/works/total.blade.php
@@ -1,21 +1,45 @@
 @extends('layouts.main')
 
 @section('title', __('translates.navbar.work'))
+
 @section('style')
     <style>
-        .table td, .table th{
+        .table td,
+        .table th {
             vertical-align: middle !important;
         }
+
         .table tr {
             cursor: pointer;
         }
+
         .hiddenRow {
             padding: 0 4px !important;
             background-color: #eeeeee;
             font-size: 13px;
         }
-        .table{
+
+        .table {
             overflow-x: scroll;
+        }
+
+        /* Stil değişiklikleri */
+        .work-stats {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 20px;
+        }
+
+        .work-stats h1 {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        .work-stats h2 {
+            font-size: 18px;
+            font-weight: normal;
+            margin-bottom: 10px;
         }
     </style>
 @endsection
@@ -30,15 +54,23 @@
         </x-bread-crumb-link>
     </x-bread-crumb>
 
+    <div class="work-stats">
+        <div>
+            <h1>Ayın Başından Qeyri-Rəsmi Məbləğ</h1>
+            <h2>{{ $totalIllegalAmount }}</h2>
+        </div>
+        <div>
+            <h1>Ayın Başından Rəsmi Məbləğ</h1>
+            <h2>{{ $totalAmount }}</h2>
+        </div>
+        <div>
+            <h1>Ayın Başından ƏDV Məbləğ</h1>
+            <h2>{{ $totalVat }}</h2>
+        </div>
+    </div>
 
-    <h1>Ayın Əvvəlindən qeyri-rəsmi məbləğ</h1>
-    <h2>{{ $totalIllegalAmount }}</h2>
-    <h1>Ayın Əvvəlindən rəsmi məbləğ</h1>
-    <h2>{{ $totalAmount }}</h2>
-    <h1>Ayın Əvvəlindən ədv məbləğ</h1>
-    <h2>{{ $totalVat }}</h2>
-
+    <!-- Diğer içerikler -->
 @endsection
+
 @section('scripts')
 @endsection
-
