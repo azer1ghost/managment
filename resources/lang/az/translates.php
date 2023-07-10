57536dd73cfe430ea883ead550c44594832f1e66
Valiyev <aliveliyev016@gmail.com>
2023-07-10 12:14:24 +0400

    work

--- resources/lang/az/translates.php
+++ resources/lang/az/translates.php
@@ -720,7 +720,7 @@ return [
         'Qəbul edilmyib',
     ],
 
-    'work_destination' => [ 'Seçilməyib',
+    'work_destination' => [
         1 => '14000 Aksizli mallar üzrə BGİ',
         '00100 Bakı BGİ',
         '00800 HNBGİ',
