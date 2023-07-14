091ac5c5b2db2065f3841accd2cb230b98870f7d
Valiyev <aliveliyev016@gmail.com>
2023-07-14 16:20:43 +0400

    customer engagement

--- resources/views/pages/customer-engagements/index.blade.php
+++ resources/views/pages/customer-engagements/index.blade.php
@@ -94,6 +94,12 @@
                         <td>
                             @if(in_array($customer_engagement->getRelationValue('user')->getAttribute('id'), [20, 86, 22, 154, 41]) && $customer_engagement->getRelationValue('client')->created_at > '2023-06-01 00:00:00')
                                 {{$customer_engagement->getAttribute('amount')*0.15}}
+                            @elseif(in_array($customer_engagement->getRelationValue('user')->getAttribute('id'), [51]))
+                                {{$customer_engagement->getAttribute('amount')*0.15}}
+                            @elseif(in_array($customer_engagement->getRelationValue('user')->getAttribute('id'), [141]))
+                                {{$customer_engagement->getAttribute('amount')*0.20}}
+                            @elseif(in_array($customer_engagement->getRelationValue('user')->getAttribute('id'), [156]))
+                                {{$customer_engagement->getAttribute('amount')*0.30}}
                             @else
                                 {{$customer_engagement->getAttribute('amount')*0.10}}
                             @endif
@@ -132,12 +138,17 @@
                     <th colspan="7"></th>
                     <th>Toplam Bonus:</th>
                     <th>
-                        @if(in_array($customer_engagement->getRelationValue('user')->getAttribute('id'), [20, 86, 22, 154]) && $customer_engagement->getRelationValue('client')->created_at > '2023-06-01 00:00:00')
+                        @if(in_array($customer_engagement->getRelationValue('user')->getAttribute('id'), [20, 86, 22, 154, 41]) && $customer_engagement->getRelationValue('client')->created_at > '2023-06-01 00:00:00')
                             {{ $customer_engagements->sum('amount') * 0.15 }}
+                        @elseif(in_array($customer_engagement->getRelationValue('user')->getAttribute('id'), [51]))
+                            {{ $customer_engagements->sum('amount') * 0.15 }}
+                        @elseif(in_array($customer_engagement->getRelationValue('user')->getAttribute('id'), [141]))
+                            {{ $customer_engagements->sum('amount') * 0.20 }}
+                        @elseif(in_array($customer_engagement->getRelationValue('user')->getAttribute('id'), [156]))
+                            {{ $customer_engagements->sum('amount') * 0.30 }}
                         @else
                             {{ $customer_engagements->sum('amount') * 0.10 }}
                         @endif
-
                     </th>
                     <th></th>
                 </tr>
