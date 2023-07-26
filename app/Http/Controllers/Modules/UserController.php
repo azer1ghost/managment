9594cc6e005de25e73482fdb67499fcdad72fc8e
Valiyev <aliveliyev016@gmail.com>
2023-07-26 10:27:36 +0400

    users

--- app/Http/Controllers/Modules/UserController.php
+++ app/Http/Controllers/Modules/UserController.php
@@ -191,7 +191,7 @@ class UserController extends Controller
             $totalqib += $work->getParameter(Work::GB);
         }
         foreach ($representation as $work) {
-            $totalrepresentation += $work->getParameter(Work::AMOUNT + Work::ILLEGALAMOUNT);
+            $totalrepresentation += $work->getParameter((Work::AMOUNT + Work::ILLEGALAMOUNT));
         }
 
         return view('pages.users.edit')
