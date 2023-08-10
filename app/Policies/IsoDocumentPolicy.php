10808e3b978ece7f91a7dfdbedc4ccf972165add
Valiyev <aliveliyev016@gmail.com>
2023-08-10 15:04:14 +0400

    summits

--- app/Policies/IsoDocumentPolicy.php
+++ app/Policies/IsoDocumentPolicy.php
@@ -23,12 +23,12 @@ class IsoDocumentPolicy
 
     public function create(User $user): bool
     {
-        return $this->canManage($user, 'isoDocument');
+        return $this->canManage($user, 'isoDocument', __FUNCTION__);
     }
 
     public function update(User $user): bool
     {
-        return $this->canManage($user, 'isoDocument');
+        return $this->canManage($user, 'isoDocument', __FUNCTION__);
     }
 
     public function delete(User $user): bool
