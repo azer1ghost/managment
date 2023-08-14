c6a7ba4f0fc193005638ce1eb0547ef8533c01d9
Valiyev <aliveliyev016@gmail.com>
2023-08-14 15:59:31 +0400

    total

--- app/Http/Controllers/Modules/WorkController.php
+++ app/Http/Controllers/Modules/WorkController.php
@@ -860,13 +860,23 @@ class WorkController extends Controller
 
 //        $totalPaidAll = $totalPaidAmount + $totalPaidVat + $totalPaidIllegal;
 
-        $AMBGIASAZACash = $works->where('department_id', 11)->whereIn('asan_imza_id', [39, 55])->where('payment_method', 1)->sum->getParameter(Work::AMOUNT + Work::ILLEGALAMOUNT + Work::VAT);
-        $AMBGIMOBILCash = $works->where('department_id', 11)->whereIn('asan_imza_id', [3, 51, 44, 38, 35, 31, 28, 26, 19, 5, 4])->where('payment_method', 1)->sum->getParameter(Work::AMOUNT + Work::ILLEGALAMOUNT + Work::VAT);
-        $AMBGIGARANTCash = $works->where('department_id', 11)->whereIn('asan_imza_id', [8, 9, 10, 25, 27, 45, 52])->where('payment_method', 1)->sum->getParameter(Work::AMOUNT + Work::ILLEGALAMOUNT + Work::VAT);
-        $AMBGIRIGELCash = $works->where('department_id', 11)->whereIn('asan_imza_id', [30, 32, 41, 43, 54])->where('payment_method', 1)->sum->getParameter(Work::AMOUNT + Work::ILLEGALAMOUNT + Work::VAT);
-        $AMBGIMINDCash = $works->where('department_id', 11)->whereIn('asan_imza_id', [29, 34, 36, 40, 46, 53])->where('payment_method', 1)->sum->getParameter(Work::AMOUNT + Work::ILLEGALAMOUNT + Work::VAT);
-        $AMBGITEDORACash = $works->where('department_id', 11)->whereIn('asan_imza_id', [48, 57])->where('payment_method', 1)->sum->getParameter(Work::AMOUNT + Work::ILLEGALAMOUNT + Work::VAT);
-        $AMBGIDECLARECash = $works->where('department_id', 11)->whereIn('asan_imza_id', [47, 49, 50, 56])->where('payment_method', 1)->sum->getParameter(Work::AMOUNT + Work::ILLEGALAMOUNT + Work::VAT);
+        $calculateTotal = function ($asan_imza_ids) use ($works) {
+            return $works->where('department_id', 11)
+                ->whereIn('asan_imza_id', $asan_imza_ids)
+                ->where('payment_method', 1)
+                ->sum(function ($work) {
+                    return $work->getAttribute(Work::AMOUNT) +
+                        $work->getAttribute(Work::ILLEGALAMOUNT) +
+                        $work->getAttribute(Work::VAT);
+                });
+        };
+        $AMBGIASAZACash = $calculateTotal([39, 55]);
+        $AMBGIMOBILCash = $calculateTotal([3, 51, 44, 38, 35, 31, 28, 26, 19, 5, 4]);
+        $AMBGIGARANTCash = $calculateTotal([8, 9, 10, 25, 27, 45, 52]);
+        $AMBGIRIGELCash = $calculateTotal([30, 32, 41, 43, 54]);
+        $AMBGIMINDCash = $calculateTotal([29, 34, 36, 40, 46, 53]);
+        $AMBGITEDORACash = $calculateTotal([48, 57]);
+        $AMBGIDECLARECash = $calculateTotal([47, 49, 50, 56]);
 
         $AMBGIPaidIllegal = $works->where('department_id', 11)->sum->getParameter(Work::ILLEGALPAID);
         $AMBGIPaidVat = $works->where('department_id', 11)->sum->getParameter(Work::VATPAYMENT);
