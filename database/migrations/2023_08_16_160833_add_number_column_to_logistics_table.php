25b1b5718a95e0591f4676444de911722243ef15
Valiyev <aliveliyev016@gmail.com>
2023-08-17 16:33:10 +0400

    works

--- database/migrations/2023_08_16_160833_add_number_column_to_logistics_table.php
+++ database/migrations/2023_08_16_160833_add_number_column_to_logistics_table.php
@@ -14,7 +14,7 @@ class AddNumberColumnToLogisticsTable extends Migration
     public function up()
     {
         Schema::table('logistics', function (Blueprint $table) {
-            $table->integer('number')->nullable();
+            $table->integer('number1')->nullable();
         });
     }
 
