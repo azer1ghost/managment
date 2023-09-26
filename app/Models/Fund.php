ff5ff6cb57d8be4d3a520f718784a9ec4e44c72e
Valiyev <aliveliyev016@gmail.com>
2023-09-26 17:13:06 +0400

    funds

--- app/Models/Fund.php
+++ app/Models/Fund.php
@@ -19,11 +19,11 @@ class Fund extends Model implements Recordable, DocumentableInterface
 
     public function users(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class)->withDefault();
     }
     public function companies(): BelongsTo
     {
-        return $this->belongsTo(Company::class, 'company_id');
+        return $this->belongsTo(Company::class, )->withDefault();
     }
     public function getMainColumn(): string
     {
