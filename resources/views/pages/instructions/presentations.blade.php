876ed59dde248459a63afd5caf3bc0c380b79e03
Valiyev <aliveliyev016@gmail.com>
2023-10-06 15:39:18 +0400

    presentations

--- resources/views/pages/instructions/presentations.blade.php
+++ resources/views/pages/instructions/presentations.blade.php
@@ -26,23 +26,23 @@
 
             <h2 data-toggle="collapse" href="#broker">Mobil Broker</h2>
             <div class="collapse" id="broker">
-                <a href="{{asset('assets/images/presentations/M.Broker - AZE - edit (2).pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Broker Az</i></a>
-                <a href="{{asset('assets/images/presentations/Mobil Broker - ENG (2).pdf')}}" class="m-3"><i class="far fa-file-pdf fa-3x text-primary">Mobil Broker En</i></a>
-                <a href="{{asset('assets/images/presentations/Mobil Broker - RUS (2) - edit.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Broker Rus</i></a>
+                <a href="{{asset('assets/images/presentations/Mobil Broker kataloq done.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Broker Az</i></a>
+                <a href="{{asset('assets/images/presentations/Mobil Broker english done.pdf')}}" class="m-3"><i class="far fa-file-pdf fa-3x text-primary">Mobil Broker En</i></a>
+                <a href="{{asset('assets/images/presentations/Mobil Broker - RUS (1) done.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Broker Rus</i></a>
             </div>
 
             <h2 data-toggle="collapse" href="#logistics">Mobil Logistics</h2>
             <div class="collapse" id="logistics">
-                <a href="{{asset('assets/images/presentations/Mobil Logistics - AZE (2).pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Logistics Az</i></a>
-                <a href="{{asset('assets/images/presentations/Mobil Logistics - ENG (2) - edit.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Logistics En</i></a>
-                <a href="{{asset('assets/images/presentations/Mobil Logistics - RUS (2) - edit.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Logistics Ru</i></a>
+                <a href="{{asset('assets/images/presentations/Mobil Logistics kataloq aze done.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Logistics Az</i></a>
+                <a href="{{asset('assets/images/presentations/Mobil Logistics - ENG done.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Logistics En</i></a>
+                <a href="{{asset('assets/images/presentations/Mobil Logistics - RUS done.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Logistics Ru</i></a>
             </div>
 
             <h2 data-toggle="collapse" href="#group">Mobil Group</h2>
             <div class="collapse justify-content-center" id="group">
-                <a href="{{asset('assets/images/presentations/Mobil Group - AZE (2) - edit.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Group Az</i></a>
-                <a href="{{asset('assets/images/presentations/Mobil Group - ENG (2) - edit.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Group En</i></a>
-                <a href="{{asset('assets/images/presentations/Mobil Group - RUS (2) - edit.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Group Ru</i></a>
+                <a href="{{asset('assets/images/presentations/Mobil Group - AZE kataloq done.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Group Az</i></a>
+                <a href="{{asset('assets/images/presentations/Mobil Group eng kataloq DONE.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Group En</i></a>
+                <a href="{{asset('assets/images/presentations/Mobil Group - RUS kataloq done.pdf')}}" class="text-primary m-3"><i class="far fa-file-pdf fa-3x">Mobil Group Ru</i></a>
             </div>
         </div>
     </div>
