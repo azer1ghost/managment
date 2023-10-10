74c54faf1e5b0e1f50bbb4efd311ba25df3da231
Valiyev <aliveliyev016@gmail.com>
2023-10-10 15:47:18 +0400

    permission

--- app/Http/Controllers/Modules/UserController.php
+++ app/Http/Controllers/Modules/UserController.php
@@ -136,8 +136,8 @@ class UserController extends Controller
         $totalbranchgb = 0;
         $totalbranchqib = 0;
 
-        $startOfMonth = now()->setMonth(8)->startOfMonth();
-        $endOfMonth = now()->setMonth(8)->endOfMonth();
+        $startOfMonth = now()->setMonth(10)->startOfMonth();
+        $endOfMonth = now()->setMonth(10)->endOfMonth();
 
 //        $startOfMonth = now()->startOfMonth();
 //        $endOfMonth = now()->endOfMonth();
