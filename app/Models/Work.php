eac82a7e351b6d6a097f5c915b0776f1876bedfc
Valiyev <aliveliyev016@gmail.com>
2023-10-26 12:13:22 +0400

    destination

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -136,7 +136,7 @@ class Work extends Model implements DocumentableInterface, Recordable
 
     public static function destinations(): array
     {
-        return [1 => 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+        return [1 => 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     }
 
     public static function paymentMethods(): array
