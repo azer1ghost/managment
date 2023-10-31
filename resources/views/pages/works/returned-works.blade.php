83bb4caf17c0204aff00e8520d124c1b7f8af5c0
Valiyev <aliveliyev016@gmail.com>
2023-10-31 16:30:58 +0400

    destination

--- resources/views/pages/works/returned-works.blade.php
+++ resources/views/pages/works/returned-works.blade.php
@@ -194,7 +194,7 @@
             </div>
         </div>
     @endif
-    <table class="table table-condensed-table-responsive @if($works->count()) table-responsive-md @else table-responsive-sm @endif" style="border-collapse:collapse;"  id="table">
+    <table  class="table table-condensed-table-responsive accordian-body collapse @if($works->count()) table-responsive-md @else table-responsive-sm @endif" style="border-collapse:collapse;"  id="table">
         <thead>
         <tr class="text-center">
 
