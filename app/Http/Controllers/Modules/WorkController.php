ead5c1c4dfa50142568cc59c1777e95e551013b0
Gafar <qafarzade2014@gmail.com>
2023-12-13 14:40:54 +0300

    test bug

--- app/Http/Controllers/Modules/WorkController.php
+++ app/Http/Controllers/Modules/WorkController.php
@@ -801,10 +801,10 @@ class WorkController extends Controller
         }
 
         $validated['status'] = $status;
-        if (($request->get('parameters')[$work::GB] !== $work->getParameter($work::GB)) || ($request->get('parameters')[$work::CODE] !== $work->getParameter($work::CODE)) ) {
-
-            event(new WorkChanged($work));
-        }
+//        if (($request->get('parameters')[$work::GB] !== $work->getParameter($work::GB)) || ($request->get('parameters')[$work::CODE] !== $work->getParameter($work::CODE)) ) {
+//
+//            event(new WorkChanged($work));
+//        }
 
         $work->update($validated);
 
