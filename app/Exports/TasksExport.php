07d226dcc88f8bb47636ce04925afaf13207633a
Valiyev <aliveliyev016@gmail.com>
2024-04-01 11:00:02 +0400

    works

--- app/Exports/TasksExport.php
+++ app/Exports/TasksExport.php
@@ -14,7 +14,7 @@ class TasksExport implements FromCollection, WithHeadings, WithMapping
     */
     public function collection()
     {
-        $startDate = '2024-01-01';
+        $startDate = '2024-03-01';
         $endDate = '2024-12-31';
 
         return Task::whereBetween('created_at', [$startDate, $endDate])->get();
