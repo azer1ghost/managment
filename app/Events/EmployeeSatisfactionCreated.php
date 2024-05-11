7e6bff7107e3810c0ddf02812150c7f99cbd034f
Valiyev <aliveliyev016@gmail.com>
2024-05-11 11:32:34 +0400

    employee satisfaction notification

--- app/Events/EmployeeSatisfactionCreated.php
+++ app/Events/EmployeeSatisfactionCreated.php
@@ -21,15 +21,15 @@ class EmployeeSatisfactionCreated
         $this->title = trans('translates.employee_satisfactions.incompatibility',['types' => trans('translates.employee_satisfactions.types.' . $employeeSatisfaction->getAttribute('type'))] );
         $this->body = strip_tags($employeeSatisfaction->getAttribute('content'));
         $directors = User::where('role_id', User::DIRECTOR)->orWhere('department_id', 25)->orWhere('id', [123, 78])->get()->all();
-        $department = $employeeSatisfaction->getRelationValue('departments')->users()
-            ->whereNotIn('id', [auth()->id()])
-            ->get()->all();
+//        $department = $employeeSatisfaction->getRelationValue('departments')->users()
+//            ->whereNotIn('id', [auth()->id()])
+//            ->get()->all();
         if ($employeeSatisfaction->getAttribute('department_id') == null) {
             if ($employeeSatisfaction->getAttribute('user_id') !== null) {
                 $this->receivers = $directors;
             }
         } else {
-            $this->receivers = array_merge($directors, $department);
+            $this->receivers = array_merge($directors);
         }
     }
 }
