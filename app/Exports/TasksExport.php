7fa501e6eaf7792933105d95c221a9187d24eb6b
gafar_zade <qafarzade2014@gmail.com>
2024-05-24 16:18:07 +0400

    export

--- app/Exports/TasksExport.php
+++ app/Exports/TasksExport.php
@@ -29,7 +29,7 @@ class TasksExport implements FromCollection, WithHeadings, WithMapping
             'Tapşırıq verən',
             'Şöbə',
             'İstifadəçi',
-            'Son tarix',
+            'Bitme Tarixi',
             'Nəticə'
         ];
     }
