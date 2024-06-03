9fefc2fd13dff039515fcce570fae3a7535325a7
Valiyev <aliveliyev016@gmail.com>
2024-06-03 15:45:20 +0400

    inquiries export

--- app/Exports/TasksExport.php
+++ app/Exports/TasksExport.php
@@ -15,7 +15,7 @@ class TasksExport implements FromCollection, WithHeadings, WithMapping
     public function collection()
     {
         $startDate = '2024-05-01';
-        $endDate = '2024-05-31 ';
+        $endDate = '2024-05-31';
 
         return Task::whereBetween('created_at', [$startDate, $endDate])->whereIn('user_id', [187, 102])->get();
     }
