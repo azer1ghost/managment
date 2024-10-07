5d91bc2a07f54e1cb093e523560259c8df2ed07e
Veliyev <aliveliyev016@gmail.com>
2024-10-07 14:41:05 +0400

    sales

--- app/View/Components/Widgets/UserWorkMonthlyWidget.php
+++ app/View/Components/Widgets/UserWorkMonthlyWidget.php
@@ -21,6 +21,9 @@ class UserWorkMonthlyWidget extends Component
         $this->widget = $widget;
         $this->model = $this->getClassRealName();
 
+        $userId = auth()->id();
+        $specialUserIds = [17, 124, 15, 123];
+
         $this->works = DB::table('users')
             ->select('users.name', 'users.surname', DB::raw('SUM(work_parameter.value) AS total_value'))
             ->join('works', 'works.user_id', '=', 'users.id')
@@ -28,10 +31,19 @@ class UserWorkMonthlyWidget extends Component
             ->where('work_parameter.parameter_id', '=', Work::GB)
             ->where('disabled_at', '=', null)
             ->where('works.deleted_at', '=', null)
-            ->whereDate('entry_date', '>=', now()->startOfMonth())
-            ->groupBy('users.name', 'users.surname')
-            ->orderByDesc('total_value')
-            ->get();
+            ->whereDate('entry_date', '>=', now()->startOfMonth());
+
+
+        if (in_array($userId, $specialUserIds)) {
+            $this->works = $this->works->get();
+        } else {
+            $departmentId = DB::table('users')->where('id', $userId)->value('department_id');
+
+            $this->works = $this->works
+                ->where('users.department_id', '=', $departmentId)
+                ->get();
+        }
+
     }
 
     public function render()
