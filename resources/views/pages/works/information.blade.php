1ece4004c96f16ff4190a90a7fc7d60aaf3e27f4
Veliyev <aliveliyev016@gmail.com>
2024-10-10 11:00:31 +0400

    sales

--- resources/views/pages/works/information.blade.php
+++ resources/views/pages/works/information.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.main')
 
 @section('title', __('translates.navbar.information'))
+@section('style')
+    <style>
+        /* Tabloyu kapsayan container */
+        .datatable-container {
+            width: 100%;
+            overflow-x: auto;
+        }
 
+        /* Tablo genişliğini sabitleme */
+        table.dataTable {
+            table-layout: fixed;
+            width: 100% !important;
+        }
+
+        /* Hücrelerin taşmasını önlemek ve kaydırma eklemek */
+        table.dataTable th,
+        table.dataTable td {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+    </style>
+
+@endsection
 @section('content')
     <x-bread-crumb>
         <x-bread-crumb-link :link="route('dashboard')">
@@ -84,17 +107,27 @@
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 
     <script>
-
         $(document).ready(function() {
             $('#works-table').DataTable({
-                "paging": true,
-                "searching": true,
-                "ordering": true,
-                "info": true,
-                "autoWidth": false,
-                "order": []
+                "paging": true, // Sayfalama
+                "searching": true, // Arama
+                "ordering": true, // Sıralama
+                "info": true, // Bilgi gösterimi
+                "autoWidth": false, // Otomatik genişlik kapalı
+                "scrollX": true, // Yatay kaydırma ekler
+                "fixedHeader": true, // Başlıkların sabit kalmasını sağlar
+                "order": [], // Varsayılan sıralama yok
+                "columnDefs": [
+                    { "width": "150px", "targets": 0 }, // Her sütun için genişlik ayarı
+                    { "width": "150px", "targets": 1 },
+                    { "width": "200px", "targets": 2 },
+                    { "width": "250px", "targets": 3 },
+                    { "width": "250px", "targets": 4 },
+                    { "width": "250px", "targets": 5 }
+                ]
             });
         });
     </script>
+
 
 @endsection
