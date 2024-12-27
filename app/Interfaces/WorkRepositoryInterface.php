2528271af5995d16584d0f6de3272f746fa77ede
Veliyev <aliveliyev016@gmail.com>
2024-12-27 14:22:23 +0400

    export issue

--- app/Interfaces/WorkRepositoryInterface.php
+++ app/Interfaces/WorkRepositoryInterface.php
@@ -4,9 +4,5 @@ namespace App\Interfaces;
 
 interface WorkRepositoryInterface
 {
-    public function allFilteredWorks(array $filters = [], array $dateFilters = []): \Illuminate\Database\Eloquent\Builder;
-
-    public function getWorksWithSpecificColumns(array $columns): \Illuminate\Database\Eloquent\Collection;
-
-    public function countFilteredWorks(array $filters = [], array $dateFilters = []): int;
+    public function allFilteredWorks(array $filters = [], array $dateFilters = []);
 }
--- app/Repositories/WorkRepository.php
+++ app/Repositories/WorkRepository.php
@@ -6,67 +6,105 @@ use App\Interfaces\WorkRepositoryInterface;
 use App\Models\Work;
 use Carbon\Carbon;
 
-class WorkRepository implements WorkRepositoryInterface
-{
-    public function allFilteredWorks(array $filters = [], array $dateFilters = []): \Illuminate\Database\Eloquent\Builder
+class WorkRepository implements WorkRepositoryInterface {
+
+    public function allFilteredWorks(array $filters = [], $dateFilters = [])
     {
-        $query = Work::query();
+        $user = auth()->user();
+        $dateRanges = [
+            'datetime' => explode(' - ', $filters['datetime']),
+            'created_at' => explode(' - ', $filters['created_at']),
+            'injected_at' => explode(' - ', $filters['injected_at']),
+            'entry_date' => explode(' - ', $filters['entry_date']),
+            'invoiced_date' => explode(' - ', $filters['invoiced_date']),
+            'vat_date' => explode(' - ', $filters['vat_date']),
+        ];
+        $status = $filters['status'];
+        $statuses = $filters['statuses'] ?? [];
 
-        $this->applyFilters($query, $filters);
-
-        $this->applyDateFilters($query, $dateFilters);
-
-        if (isset($filters['limit']) && is_numeric($filters['limit'])) {
-            $query->take($filters['limit']);
-        }
-
-        return $query;
-    }
-
-
-    public function getWorksWithSpecificColumns(array $columns): \Illuminate\Database\Eloquent\Collection
-    {
-        return Work::select($columns)->get();
-    }
-
-    public function countFilteredWorks(array $filters = [], array $dateFilters = []): int
-    {
-        return $this->allFilteredWorks($filters, $dateFilters)->count();
-    }
-
-    protected function applyFilters(&$query, array $filters): void
-    {
-        foreach ($filters as $key => $value) {
-            if (!empty($value)) {
-                if ($key === 'limit') {
-                    continue;
+        return Work::query()
+            ->with([
+                'creator','department:id,name,short_name',
+                'service',
+                'user:id,name,surname,department_id,permissions',
+                'client:id,fullname,voen',
+                'asanImza:id,user_id,company_id'
+            ])
+            ->when(!empty($status), function ($query) use ($status) {
+                $query->where('status', $status);
+            })
+            ->when(!empty($statuses), function ($query) use ($statuses) {
+                $query->whereNotIn('status', $statuses);
+            })
+            ->when(Work::userCannotViewAll(), function ($query) use ($user){
+                if($user->hasPermission('viewAllDepartment-work')){
+                    $query->where('department_id', $user->getAttribute('department_id'));
+                }else{
+                    $query
+                        ->where(function ($q) use ($user){
+                            $q->whereNull('user_id')->where('department_id', $user->getAttribute('department_id'));
+                        })
+                        ->orWhere('user_id', $user->getAttribute('id'));
                 }
-                if ($key === 'statuses') {
-                    $query->whereIn('status', $value);
-                    continue;
+            })
+            ->where(function($query) use ($filters, $dateRanges, $dateFilters){
+                foreach ($filters as $column => $value) {
+                    if($column == 'limit') continue;
+                    $query->when($value, function ($query, $value) use ($column, $dateRanges, $dateFilters) {
+                        if($column == 'verified_at'){
+                            switch ($value){
+                                case 1:
+                                    $query->whereNull($column);
+                                    break;
+                                case 2:
+                                    $query->whereNotNull($column);
+                                    break;
+                            }
+                        }
+                        else if ($column == 'paid_at'){
+                            switch ($value){
+                                case 1:
+                                    $query->whereNull($column);
+                                    break;
+                                case 2:
+                                    $query->whereNotNull($column);
+                                    break;
+                            }
+                        }
+                        else if($column == 'asan_imza_company_id'){
+                            $query->whereHas('asanImza', function ($asanImzaQuery) use ($value) {
+                                $asanImzaQuery->whereHas('company', function ($companyQuery) use ($value) {
+                                    $companyQuery->whereId($value);
+                                });
+                            });
+                        }else{
+                            if($column == 'code'){
+                                $query->where($column, 'LIKE', "%$value%");
+                            }
+                            elseif($column == 'declaration_no'){
+                                $query->where($column, 'LIKE', "%$value%");
+                            }
+                            elseif($column == 'service_id'){
+                                $query->whereIn($column, $value);
+                            }
+                            else if (is_numeric($value)){
+                                $query->where($column, $value);
+                            }
+                            else if(is_string($value) && $dateFilters[$column]){
+                                $query->whereBetween($column,
+                                    [
+                                        Carbon::parse($dateRanges[$column][0])->startOfDay(),
+                                        Carbon::parse($dateRanges[$column][1])->endOfDay()
+                                    ]
+                                );
+                            }
+                        }
+                    });
                 }
-                if (is_array($value)) {
-                    $query->whereIn($key, $value);
-                } elseif (is_string($value)) {
-                    $query->where($key, 'LIKE', "%$value%");
-                } else {
-                    $query->where($key, $value);
-                }
-            }
-        }
+            })
+            ->latest('id')
+            ->latest('datetime');
+//            ->orderBy('status')
+//        ->orderByDesc('id');
     }
-
-
-    protected function applyDateFilters(&$query, array $dateFilters): void
-    {
-        foreach ($dateFilters as $key => $dateRange) {
-            if (!empty($dateRange) && is_array($dateRange) && count($dateRange) === 2) {
-                $query->whereBetween($key, [
-                    Carbon::parse($dateRange[0])->startOfDay(),
-                    Carbon::parse($dateRange[1])->endOfDay(),
-                ]);
-            }
-        }
-    }
-
 }
