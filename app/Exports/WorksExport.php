35d1bbb26e19b2b50eb43e2e181fbc42eb7a4222
Veliyev <aliveliyev016@gmail.com>
2025-02-15 13:17:49 +0400

    departments

--- app/Exports/WorksExport.php
+++ app/Exports/WorksExport.php
@@ -30,28 +30,21 @@ class WorksExport implements FromQuery, WithMapping, WithHeadings, WithColumnWid
         $this->headings = [
             'Sorğu nömrəsi',
             'İş Kodu',
-            trans('translates.columns.created_at'),
+            'Yaradan şəxs',
+            'Şöbə',
+            'Koordinator',
             'Müştəri adı',
             'Şəxs',
-            trans('translates.general.work_service'),
-
+            'Xidmət',
+            'Təyinat Orqanı',
+            'Status',
+            'Sənədlər',
+            'GB Sayı',
+            'Kod Sayı',
+            'Əsas Məbləğ Ödəniş Tarixi',
+            'ƏDV Ödəniş Tarixi',
+            'Qalıq',
         ];
-
-        foreach (Service::serviceParametersExport() as $servicesParameter) {
-            $this->headings[] = $servicesParameter['data']->getAttribute('label');
-        }
-
-        $this->headings = array_merge($this->headings, [
-                'Əsas Məbləğ Ödəniş Tarixi',
-                'Ədv Ödəniş Tarixi',
-                'Borc',
-                'Bəyannaməçi',
-                'Sistemdə (ASAN IMZA)',
-                trans('translates.columns.department'),
-                'Ödəniş Üsulu',
-                'Bank Xərci',
-            ]
-        );
     }
 
     public function headings(): array
@@ -61,35 +54,29 @@ class WorksExport implements FromQuery, WithMapping, WithHeadings, WithColumnWid
 
     public function map($row): array
     {
-        $maps = [
+        return [
             $row->getAttribute('declaration_no'),
             $row->getAttribute('code'),
-            $row->getAttribute('created_at')->format('d-m-Y'),
+            $row->getRelationValue('creator')->getAttribute('fullname_with_position'),
+            $row->getRelationValue('department')->getAttribute('short'),
+            optional($row->getRelationValue('client')->coordinators->first())->fullname ?? 'Koordinator Yoxdur',
             $row->getRelationValue('client')->getAttribute('fullname'),
             $row->getRelationValue('client')->getAttribute('type') ? 'FŞ' : 'HŞ',
-            $row->getRelationValue('service')->getAttribute('shortName')
-        ];
-        foreach (Service::serviceParametersExport() as $servicesParameter) {
-            $maps[] = $row->getParameter($servicesParameter['data']->getAttribute('id'));
-        }
-
-        return array_merge($maps, [
+            $row->getRelationValue('service')->getAttribute('name'),
+            trans('translates.work_destination.' . $row->getAttribute('destination')) ?? 'Təyinat orqanı boşdur',
+            trans('translates.work_status.' . $row->getAttribute('status')),
+            implode(', ', $row->documents->pluck('name')->toArray()),
+            $row->getParameter($row::GB),
+            $row->getParameter($row::CODE),
             optional($row->getAttribute('paid_at'))->format('d-m-Y') ?? 'Tam Ödəniş olmayıb',
             optional($row->getAttribute('vat_date'))->format('d-m-Y') ?? 'ƏDV Ödənişi olmayıb',
             ($row->getParameter($row::VAT) + $row->getParameter($row::AMOUNT) + $row->getParameter($row::ILLEGALAMOUNT) - ($row->getParameter($row::PAID) + $row->getParameter($row::VATPAYMENT) +  $row->getParameter($row::ILLEGALPAID))) * -1,
-            $row->getRelationValue('user')->getAttribute('fullname'),
-            $row->asanImza()->exists() ? $row->getRelationValue('asanImza')->getAttribute('user_with_company') : trans('translates.filters.select'),
-            $row->getRelationValue('department')->getAttribute('short'),
-            $row->getAttribute('payment_method') ? trans('translates.payment_methods.' . $row->getAttribute('payment_method')) : '',
-            $row->getAttribute('bank_charge')
-
-        ]);
+        ];
     }
 
     public function styles(Worksheet $sheet): array
     {
         return [
-            // Style the first row as bold text.
             1 => [
                 'font' => ['bold' => true],
                 'alignment' => [
@@ -102,9 +89,9 @@ class WorksExport implements FromQuery, WithMapping, WithHeadings, WithColumnWid
                     ],
                 ],
             ],
-
         ];
     }
+
     public function query()
     {
         return $this->workRepository->allFilteredWorks($this->filters, $this->dateFilters);
@@ -113,16 +100,11 @@ class WorksExport implements FromQuery, WithMapping, WithHeadings, WithColumnWid
     public function columnWidths(): array
     {
         return [
-            'E' => 35,
-            'F' => 13,
+            'B' => 20,
+            'C' => 25,
+            'D' => 15,
+            'E' => 30,
+            'F' => 35,
         ];
     }
-
-//    public function columnFormats(): array
-//    {
-//        return [
-//            'B' => NumberFormat::FORMAT_DATE_DDMMYYYY,
-//            'C' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
-//        ];
-//    }
 }
