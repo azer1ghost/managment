fa1f2227e33b9f11d6376f3c1461ea481b82d712
Veliyev <aliveliyev016@gmail.com>
2025-04-11 12:40:08 +0400

    export

--- app/Repositories/WorkRepository.php
+++ app/Repositories/WorkRepository.php
@@ -25,8 +25,7 @@ class WorkRepository implements WorkRepositoryInterface {
         return Work::query()
             ->select('works.*')
             ->with([
-                'creator',
-                'department:id,name,short_name',
+                'creator','department:id,name,short_name',
                 'service',
                 'user:id,name,surname,department_id,permissions',
                 'client:id,fullname,voen',
@@ -39,67 +38,77 @@ class WorkRepository implements WorkRepositoryInterface {
                 $query->whereNotIn('status', $statuses);
             })
             ->when(Work::userCannotViewAll(), function ($query) use ($user){
-                if ($user->hasPermission('viewAllDepartment-work')) {
+                if($user->hasPermission('viewAllDepartment-work')){
                     $query->where('department_id', $user->getAttribute('department_id'));
-                } else {
+                }else{
                     $query
                         ->where(function ($q) use ($user){
-                            $q->whereNull('user_id')
-                                ->where('department_id', $user->getAttribute('department_id'));
+                            $q->whereNull('user_id')->where('department_id', $user->getAttribute('department_id'));
                         })
                         ->orWhere('user_id', $user->getAttribute('id'));
                 }
             })
             ->where(function($query) use ($filters, $dateRanges, $dateFilters){
                 foreach ($filters as $column => $value) {
-                    // 'limit' və 'statuses' filterə salınmasın
-                    if (in_array($column, ['limit', 'statuses'])) continue;
-
-                    $query->when(isset($value), function ($query) use ($column, $value, $dateRanges, $dateFilters) {
-                        // Tarixlər
-                        if (isset($dateFilters[$column]) && isset($dateRanges[$column])) {
-                            return $query->whereBetween($column, [
-                                Carbon::parse($dateRanges[$column][0])->startOfDay(),
-                                Carbon::parse($dateRanges[$column][1])->endOfDay()
-                            ]);
+                    if($column == 'limit') continue;
+                    $query->when($value, function ($query, $value) use ($column, $dateRanges, $dateFilters) {
+                        if($column == 'verified_at'){
+                            switch ($value){
+                                case 1:
+                                    $query->whereNull($column);
+                                    break;
+                                case 2:
+                                    $query->whereNotNull($column);
+                                    break;
+                            }
                         }
-
-                        // Null / NotNull
-                        if (in_array($column, ['verified_at', 'paid_at'])) {
-                            return $value == 1
-                                ? $query->whereNull($column)
-                                : $query->whereNotNull($column);
+                        else if ($column == 'paid_at'){
+                            switch ($value){
+                                case 1:
+                                    $query->whereNull($column);
+                                    break;
+                                case 2:
+                                    $query->whereNotNull($column);
+                                    break;
+                            }
                         }
-
-                        // LIKE
-                        if (in_array($column, ['code', 'declaration_no'])) {
-                            return $query->where($column, 'like', "%$value%");
-                        }
-
-                        // Relation
-                        if ($column === 'asan_imza_company_id') {
-                            return $query->whereHas('asanImza.company', function ($q) use ($value) {
-                                $q->where('id', $value);
+                        else if($column == 'asan_imza_company_id'){
+                            $query->whereHas('asanImza', function ($asanImzaQuery) use ($value) {
+                                $asanImzaQuery->whereHas('company', function ($companyQuery) use ($value) {
+                                    $companyQuery->whereId($value);
+                                });
                             });
+                        }else{
+                            if($column == 'code'){
+                                $query->where($column, 'LIKE', "%$value%");
+                            }
+                            elseif($column == 'declaration_no'){
+                                $query->where($column, 'LIKE', "%$value%");
+                            }
+                            elseif($column == 'service_id'){
+                                $query->whereIn($column, $value);
+                            }
+                            elseif($column == 'destination'){
+                                $query->where($column, $value);
+                            }
+                            else if (is_numeric($value)){
+                                $query->where($column, $value);
+                            }
+                            else if(is_string($value) && $dateFilters[$column]){
+                                $query->whereBetween($column,
+                                    [
+                                        Carbon::parse($dateRanges[$column][0])->startOfDay(),
+                                        Carbon::parse($dateRanges[$column][1])->endOfDay()
+                                    ]
+                                );
+                            }
                         }
-
-                        if ($column === 'asan_imza_id') {
-                            return $query->whereHas('asanImza', function ($q) use ($value) {
-                                $q->where('id', $value);
-                            });
-                        }
-
-                        // Array
-                        if (is_array($value)) {
-                            return $query->whereIn($column, $value);
-                        }
-
-                        // Default equality
-                        return $query->where($column, $value);
                     });
                 }
             })
             ->latest('id')
             ->latest('datetime');
+//            ->orderBy('status')
+//        ->orderByDesc('id');
     }
 }
