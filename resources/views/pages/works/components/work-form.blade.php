b541f926cfa62b2ce0fa378d0b93965303de87d3
Veliyev <aliveliyev016@gmail.com>
2025-09-04 13:19:29 +0400

    declaration no

--- resources/views/pages/works/components/work-form.blade.php
+++ resources/views/pages/works/components/work-form.blade.php
@@ -45,17 +45,17 @@
                                    class="form-control"
                                    placeholder="Nəqliyyat nömrəsi daxil edin">
                         </div>
-                        @if(in_array($service_id, [2]))
-                            <div class="form-group col-12 col-md-3">
-                            <label for="mark">@lang('translates.fields.mark')</label>
-                            <input value="{{optional($data)->getAttribute('mark')}}"
-                                   type="text"
-                                   name="mark"
-                                   id="mark"
-                                   class="form-control"
-                                   placeholder="Qeydiyyat nişanı daxil edin"
-                            >
-                        </div> @endif
+{{--                        @if(in_array($service_id, [2]))--}}
+{{--                            <div class="form-group col-12 col-md-3">--}}
+{{--                            <label for="mark">@lang('translates.fields.mark')</label>--}}
+{{--                            <input value="{{optional($data)->getAttribute('mark')}}"--}}
+{{--                                   type="text"--}}
+{{--                                   name="mark"--}}
+{{--                                   id="mark"--}}
+{{--                                   class="form-control"--}}
+{{--                                   placeholder="Qeydiyyat nişanı daxil edin"--}}
+{{--                            >--}}
+{{--                        </div> @endif--}}
 
                     <div class="form-group col-12 col-md-6" wire:ignore>
                     <label for="data-client-type">{{trans('translates.fields.clientName')}}</label><br/>
