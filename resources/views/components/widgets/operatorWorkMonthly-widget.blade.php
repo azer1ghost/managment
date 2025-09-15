0572110d21d098a232cd005edeb6ef070658e867
Veliyev <aliveliyev016@gmail.com>
2025-09-15 11:10:36 +0400

    need attention

--- resources/views/components/widgets/operatorWorkMonthly-widget.blade.php
+++ resources/views/components/widgets/operatorWorkMonthly-widget.blade.php
@@ -3,8 +3,7 @@
         <div class="card-body">
             <div class="d-flex justify-content-between">
                 <p class="card-title">{{$widget->details}}</p>
-                <h3 >GB</h3>
-                <h3 >QIB</h3>
+                <h3 style="text-align: right;">GB QIB</h3>
             </div>
             <p class="font-weight-500"></p>
             <div></div>
